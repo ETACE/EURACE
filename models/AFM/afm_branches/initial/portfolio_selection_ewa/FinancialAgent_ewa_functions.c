9ef6cb49d181cf274b08f75d4fddeb5698bc54cf
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2007-11-19 14:47:34 +0000

    Added function dependencies to model-ewa.xml.

--- models/AFM/afm_branches/initial/portfolio_selection_ewa/FinancialAgent_ewa_functions.c
+++ models/AFM/afm_branches/initial/portfolio_selection_ewa/FinancialAgent_ewa_functions.c
@@ -24,19 +24,13 @@ int FinancialAgent_read_rule_performance_message()
 	  rule_performance_message = get_first_rule_performance_message();
 	  while(rule_performance_message)
 	  {
-	   
 	    current_rule = rule_performance_message->current_rule;
 	    rule_performance = rule_performance_message->rule_performance;
-	    
 	
 	    /* Update rule performance: */
 	    FinancialAgent_update_classifiersystem(current_rule, rule_performance);
 	    rule_performance_message = get_next_rule_performance_message(rule_performance_message)
 	  }
-	
-	    //After all updates have been read and processed: send the result
-	    //This message can be read by ALL household agents
-	    add_all_performances_message(performances, range, x, y, z);
 
    return 0;
  }
@@ -44,22 +38,15 @@ int FinancialAgent_read_rule_performance_message()
  
 int FinancialAgent_update_classifiersystem(int current_rule, double rule_performance)
 {
-  PublicClassifierSystem * classifiersystem;
   
-  //Replace old performance adding new performance: 
-  classifiersystem->array[current_rule]->performance += rule_performance;
+  //Replace old performance adding new performance: ******CHECK WHEN RESET OCCURS: SHOULD BE DAILY? 
+  CLASSIFIERSYSTEM->array[current_rule]->performance += rule_performance;
   
   //Counter update: ******CHECK WHEN RESET OCCURS: SHOULD BE DAILY?
-  classifiersystem->array[current_rule]->counter +=1;
+  CLASSIFIERSYSTEM->array[current_rule]->counter +=1;
   
   //Avgperformance update:
-  classifiersystem->array[current_rule]->avgperformance = classifiersystem->array[current_rule]->avgperformance / classifiersystem->array[current_rule]->counter;
-
-  //set_classifiersystem(classifiersystem); // setting value classifiersystem is an array need reference here
-   /*classifiersystem[current_rule]->int_ruleperformance = 9;
-    * if you're adding a rule into the structure:
-    * add_classifiersystem_(classifersystem_dynamic_array, 0,2,25);
-    */
+  CLASSIFIERSYSTEM->array[current_rule]->avgperformance = CLASSIFIERSYSTEM->array[current_rule]->avgperformance / CLASSIFIERSYSTEM->array[current_rule]->counter;
     
   return 0;
 }
@@ -69,16 +56,15 @@ int FinancialAgent_update_classifiersystem(int current_rule, double rule_perform
  */
 int FinancialAgent_send_all_performances_message()
 {
-    PublicClassifierSystem * classifiersystem;
- 	double[] all_performances;
+  	double[] all_performances;
  	 
  	//Get size of performance array:
 	imax = CLASSIFIERSYSTEM->performance->size;
 
-	//Assign values to dynamic array all_rule_performances
+	//Assign values to dynamic array all_performances to equal the average performances
 	for (i=0;i<imax;i++)
 	{
-		all_performances[i] = CLASSIFIERSYSTEM->array[i]->performance;
+		all_performances[i] = CLASSIFIERSYSTEM->array[i]->avgperformance;
 	}
 	
 	//Send the message containing the dynamic array
@@ -86,4 +72,19 @@ int FinancialAgent_send_all_performances_message()
      
   return 0;
 }
-  
+
+
+//FinancialAgent_send_ruledetailsystem_message()
+//Function to send ruledetailsystem updates.
+int FinancialAgent_send_ruledetailsystem_message()
+{
+	//Send a message containing the entire ruledetailsystem:
+	add_ruledetailsystem_message(RULEDETAILSYSTEM, range, x, y, z);
+
+	//Alternative: we only need to send the double2D_array 
+	//RULEDETAILSYSTEM->parameters
+	double** parameters = RULEDETAILSYSTEM->parameters;
+	add_ruledetailsystem_message(parameters, range, x, y, z);
+		
+    return 0;
+}
