1eee508124f02d6e33b1495fd6b72c415517add3
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2007-11-19 14:20:46 +0000

    model.xml: Added to struct RuleDetailSystem; some_new_functions.c: Done some maintenance.

--- models/AFM/afm_branches/initial/portfolio_selection_ewa/some_new_functions.c
+++ models/AFM/afm_branches/initial/portfolio_selection_ewa/some_new_functions.c
@@ -1,34 +1,16 @@
 int reset_private_classifiersystem()
 {
  	//Getting the size of the system:
-	NRRULES=CLASSIFIERSYSTEM->double_ids_dynamic_array->size;
-	NRRULES=CLASSIFIERSYSTEM->array[]->ids->size;
+	int NR_TYPES=CLASSIFIERSYSTEM->NR_TYPES;
 
-	//First declare local vars:
-    int_array[NRRULES] 		ids;
-    double					experience;
-    int						current_rule;
-    double					my_performance;
-    double_array[NRRULES] 	avgperformance;
-    double_array[NRRULES] 	attraction;
-    double_array[NRRULES] 	choiceprob;
-    
-	//Resetting:
-	CLASSIFIERSYSTEM->experience =0;
-	CLASSIFIERSYSTEM->current_rule=0;
-	CLASSIFIERSYSTEM->my_performance=0;
+	//dynamic array with number of rules in each type (size of subpopulations)
+	int* NRRULES_PER_TYPE=CLASSIFIERSYSTEM->NRRULES_PER_TYPE;
 
-	for (i=0; i<NRRULES; i++)
-	{
-	
-		CLASSIFIERSYSTEM->array[i]->avgperformance=0;
-		CLASSIFIERSYSTEM->array[i]->attraction=0;
-		CLASSIFIERSYSTEM->array[i]->choiceprob=0;
-	}
+	//total number of rules:
+	int NRRULES=CLASSIFIERSYSTEM->NRRULES;
 
 	//Resetting and storing values to memory:
-	//Using the capitalisation method to access memory values directly
-	CLASSIFIERSYSTEM->experience =0;
+	CLASSIFIERSYSTEM->experience=0;
 	CLASSIFIERSYSTEM->current_rule=0;
 	CLASSIFIERSYSTEM->my_performance=0;
 
@@ -44,30 +26,19 @@ int reset_private_classifiersystem()
 
 int reset_public_classifiersystem()
 {
-	//Not needed? then remove
-    //PublicClassifierSystem classifiersystem;
+ 	//Getting the size of the system:
+	int NR_TYPES=CLASSIFIERSYSTEM->nr_types;
 
-	//Getting the size of the system:
-	NRRULES=CLASSIFIERSYSTEM->double_ids_dynamic_array->size;
-	NRRULES=CLASSIFIERSYSTEM->array[]->ids->size;
+	//dynamic array with number of rules in each type (size of subpopulations)
+	int* NRRULES_PER_TYPE=CLASSIFIERSYSTEM->nr_rules_per_type;
 
-	//First declare local vars:
-    int_array[NRRULES] 	ids;
-    double_array[NRRULES]	performance;
-    double_array[NRRULES] 	avgperformance;
+	//total number of rules:
+	int NRRULES=CLASSIFIERSYSTEM->nr_rules;
     
-	//Resetting:
+	//Resetting and storing values to memory:
 	for (i=0; i<NRRULES; i++)
 	{
 		CLASSIFIERSYSTEM->array[i]->performance=0;
-		CLASSIFIERSYSTEM->array[i]->avgperformance=0;
-	}
-
-	//Resetting and storing values to memory:
-	//Using the capitalisation method to access memory values directly
-	for (i=0; i<NRRULES; i++)
-	{
-		CLASSIFIERSYSTEM->array[i]->performance=0;	
 		CLASSIFIERSYSTEM->array[i]->avgperformance=0;
 	}
 
@@ -79,26 +50,37 @@ int reset_public_classifiersystem()
 //NR_TYPES: number of different types of rules (typically NR_TYPES=3)
 //NRRULES_PER_TYPE[NR_TYPES]: number of rules per type (typically the same, equal to 10)
 //NRRULES_PER_TYPE=[10,10,10];
-//ids: rule ids
+//rule_type: type identifier for each rule
+//nr_params_per_type: number of parameters for each rule type
 //nr_params: no. of parameters for each rule
-//parameter: parameter setting for each rule 
-//rule_execution:  function names to execute the rule
-//rule_type: type for each rule
-//my_function_names: the names of the functions
+//parameters: parameter setting for each rule 
+//rule_execution:  function name to execute the rule
+//my_function_names: the names of the functions to call to execute a rule
 //my_function_name[1]='Markowitz';
 //my_function_name[2]='ProspectTheory';
 //my_function_name[3]='RandomRule';
 
 int initialize_ruledetailsystem()
 {
-	//Global vars:
-	int 		  		NR_TYPES;			//nr. of types of rules
-	int[NR_TYPES] 		NRRULES_PER_TYPE; 	//nr. of rules per type
-	int			  		MAX_PARAMS;
+ 	//Getting the size of the system:
+	int NR_TYPES=CLASSIFIERSYSTEM->nr_types;
+
+	//dynamic array with number of rules in each type (size of subpopulations)
+	int* NRRULES_PER_TYPE=CLASSIFIERSYSTEM->nr_rules_per_type;
+
+	//total number of rules:
+	int NRRULES=CLASSIFIERSYSTEM->nr_rules;
+	
+	//dynamic array with number of parameters in each type
+	int* nr_params_per_type=CLASSIFIERSYSTEM->nr_params_per_type;
 	
 	//Local vars:
-	int[NRRULES] 		id;
-	int[NRRULES]		nr_params;
+	int[NRRULES] 		ids;
+	
+	//Here: nr_params is the number of parameters for a rule of certain type
+	//For each type we should have a prototype and then fill the dynamic array
+	int[NRRULES]		rule_type;
+	int[NR_TYPES]		nr_params;
 	double**			parameters;			//parameter[NRRULES][MAX_PARAMS];
 	
 	char**				rule_execution;	    //rule_execution[NRRULES]
@@ -107,23 +89,28 @@ int initialize_ruledetailsystem()
 	word_array * 		my_function_names = get_my_function_names(); //list of function names
 	
 	double				param_value;
-	int					i,j,k,m;
+	int					i,j,k,m, count;
 	
+	//First we set the field 'rule_type':
+	count = 0;
+	for (i=0;i<NR_TYPES;i++)
+	{
+		//Set nr_rules in the current type
+		jmax = CLASSIFIERSYSTEM->array[i]->nr_rules_per_type;
+		
+		//For type i, traverse the rule ids from 0+count to jmax+count
+		for (j=count;j<count+jmax;j++)
+		{
+			//We are now in rule id j
+			CLASSIFIERSYSTEM->array[j]->rule_type = i;
+			CLASSIFIERSYSTEM->array[j]->nr_params = CLASSIFIERSYSTEM->array[i]->nr_params_per_type;
+			
+		}
+		//Set counter to current rule id
+		count=count+jmax;
+	}
 	
-	//Examples:
-	//Adding an int to an int dynamic array:
-	//add_int(int_var_dynamic_array, 987);
-	//Adding a model data type to a data type dynamic array
-	//add_my_datatype(my_datatype_var_dynamic_array, 4, 3.3, 2.2, 'u');
-	//Length:
-	//int_var_dynamic_array->size;
-	//Accessing: 
-	//my_datatype_var_dynamic_array->array[i]->int_datatype_var;
-	//To create a local var:
-	//int_array * my_int_list = init_int_array();
-	//Free the local var:
-	//free_int_array(my_int_list);
-	
+	//Filling in random parameter settings
 	for (i=0; i<NRRULES; i++)
 	{
 		//I want to add an empty double_array to the double2D_array parameter
@@ -145,25 +132,34 @@ int initialize_ruledetailsystem()
 
 			//param_value = p0[i] + m*dp[i];
 			//add_double(parameters[i][j], param_value);
-
 		}		
 
-		//rule_execution
+		//We set the field 'rule_execution':
 		//First possibility: we test which function name the rule has
-		rule_execution[i]=sprintf('my_function_names[%d]', rule_type[i]);
+		j=CLASSIFIERSYSTEM->array[i]->rule_type;
+		CLASSIFIERSYSTEM->array[i]->rule_execution[i]=sprintf('my_function_names[%d]',j);
 	}
 
-	//ALTERNATIVE CODE: rule_execution
+	//ALTERNATIVE CODE for rule_execution
 	//we loop over the different rule types
 	//this avoids the rule_type[i]	
-	for (h=0; h<NR_TYPES; h++)
+	count = 0;
+	for (i=0;i<NR_TYPES;i++)
 	{
-		for (i=0; i<NRRULES_PER_TYPE[h]; i++)
+		//Set nr_rules in the current type
+		jmax = CLASSIFIERSYSTEM->array[i]->nr_rules_per_type;
+		
+		//For type i, traverse the rule ids from 0+count to jmax+count
+		for (j=count;j<count+jmax;j++)
 		{
-			rule_execution[i]=sprintf('my_function_names[%d]', h);
+			//We are now in rule id j
+			//Set the function call for rule j, which is just my_function_names[i]
+			CLASSIFIERSYSTEM->array[j]->rule_execution=sprintf('my_function_names[%d]', i);
 		}
+		//Set counter to current rule id
+		count=count+jmax;
 	}
-
+	
     return 0;
 }
 
@@ -172,12 +168,12 @@ int initialize_ruledetailsystem()
 int FinancialAgent_send_ruledetailsystem_message()
 {
 	//Send a message containing the entire ruledetailsystem:
-	add_ruledetailsystem_message(ruledetailsystem, range, x, y, z);
+	add_ruledetailsystem_message(RULEDETAILSYSTEM, range, x, y, z);
 
 	//Alternative: we only need to send the double2D_array 
-	//ruledetailsystem->parameters
-	//double** parameters = ruledetailsystem->parameters;
-	//add_ruledetailsystem_message(parameters, range, x, y, z);
+	//RULEDETAILSYSTEM->parameters
+	double** parameters = RULEDETAILSYSTEM->parameters;
+	add_ruledetailsystem_message(parameters, range, x, y, z);
 		
     return 0;
 }
@@ -189,13 +185,17 @@ int FinancialAgent_send_ruledetailsystem_message()
 //parameters: changes occur due to crossover, mutation
 int Household_read_ruledetailsystem_message()
 {
-	//Global vars:
-	int 		  		NR_TYPES;			//nr. of types of rules
-	int[NR_TYPES] 		NRRULES_PER_TYPE; 	//nr. of rules per type
-	int			  		MAX_PARAMS;
+ 	//Getting the size of the system:
+	int NR_TYPES=CLASSIFIERSYSTEM->nr_types;
+
+	//dynamic array with number of rules in each type (size of subpopulations)
+	int* NRRULES_PER_TYPE=CLASSIFIERSYSTEM->nr_rules_per_type;
+
+	//total number of rules:
+	int NRRULES=CLASSIFIERSYSTEM->nr_rules;
 	
 	//Local vars:
-	int[NRRULES] 		id;
+	int[NRRULES] 		ids;
 	int[NRRULES]		nr_params;
 	double**			parameters;			//parameter[NRRULES][MAX_PARAMS];
 	
@@ -207,15 +207,15 @@ int Household_read_ruledetailsystem_message()
 	double				param_value;
 	int					i,j,k,m,jmax=0;
 
-	//HERE: As exception, we need a local copy of ruledetailsystem
-	//because we need to copy it from the message
-	//and then copy the values that are inside using a for loop.
+	//HERE: As an exception, we need a local copy of ruledetailsystem
+	//because we need to copy it from the message.
+	//And then we need to copy the values using a for loop.
 	//So we need to temporarily store the contents of the message.
-	//Do we need to free memory space afterward if we make a local copy?
+	//Do we need to free memory space afterward if we make a local copy of the struct?
 	
 	RuleDetailSystem *	ruledetailsystem;
 	
-	//Reading from new_ruledetailsystem_message
+	//Reading the new_ruledetailsystem_message
 	new_ruledetailsystem_message = get_first_new_ruledetailsystem_message();
 	while(new_ruledetailsystem_message)
 	{
@@ -228,7 +228,7 @@ int Household_read_ruledetailsystem_message()
 	for (i=0; i<NRRULES; i++)
 	{
 		//Filling the double_array parameter[i] with the parameter values of rule i
-		jmax=nr_params[i];
+		jmax=CLASSIFIERSYSTEM->array[i]->nr_params;
 		for (j=0; j<jmax; j++)
 		{
 			//Filling the field parameters[i][j]
@@ -236,7 +236,6 @@ int Household_read_ruledetailsystem_message()
 			//parameters[i] is a double_array, so to access its elements we need:
 			  parameters[i][j] = ruledetailsystem->array[i]->parameters->array[j]->(double param_value);
 		}
-
 	}
 	
     return 0;
