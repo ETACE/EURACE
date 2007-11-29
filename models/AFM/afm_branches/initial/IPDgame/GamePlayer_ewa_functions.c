3dfaaf3334a2404eb96af4838f3f3aee538a8a15
svdhoog <11057058+svdhoog@users.noreply.github.com>
2007-11-29 19:34:38 +0000

    IPD example: lots of new coding.

--- models/AFM/afm_branches/initial/IPDgame/GamePlayer_ewa_functions.c
+++ models/AFM/afm_branches/initial/IPDgame/GamePlayer_ewa_functions.c
@@ -11,32 +11,27 @@
 #include "GamePlayer_agent_header.h"
 #include "my_library_header.h"
 
-/* STEP 1. Updating performance.*/
-/* HERE: GamePlayer sends message to GameSolver agent with the per-day performance of it's own current rule, */
-int GamePlayer_send_rule_performance_message()
+/* STEP 1. Send current rule.
+ * GamePlayer_send_current_rule_message()
+ * GamePlayer sends message to GameSolver agent with its current rule.
+ */
+int GamePlayer_send_current_rule_message()
 { 
     //Declare and assign local vars
     int current_rule = CLASSIFIERSYSTEM->current_rule;
+    int id = get_id();
     
-    //Here we compute the rule performance: this function uses the performance_history
-    //since performance is computed as a time-average of capital gains obtained by the
-    //current financial investment strategy. 
-    
-    //rule_performance = calc_rule_performance(current_assetportfolio->performance_history);
-    
-    //Random performance (uses the function random_unif())
-    rule_performance = random_unif()*100;
-    
-    add_rule_performance_message(current_rule, rule_performance, range, x, y, z);
+    add_current_rule_message(id, current_rule, range, x, y, z);
 
     return 0;
 }
 
 
 /* STEP 2. Obtain information.
- * GamePlayer reads all_performances_message from GameSolver
- * Updates classifiersystem
- * Stores classifiersystem in memory
+ * GamePlayer_read_all_performances_message()
+ * GamePlayer reads all_performances_message from GameSolver.
+ * Updates classifiersystem.
+ * Stores classifiersystem in memory.
  */
 int GamePlayer_read_all_performances_message()
 {
@@ -61,8 +56,7 @@ int GamePlayer_read_all_performances_message()
 }
 
 
-/* STEP 3. Select a rule.*/
-/*
+/* STEP 3. Select a rule.
  * int GamePlayer_select_rule()
  * updates attractions
  * updates choice probabilities
@@ -151,129 +145,46 @@ int GamePlayer_select_rule()
     return 0;
 }
 
-/* GamePlayer_retrieve_rule_details()
- * Retrieve rule details
- * Copying the selected rule details to the appropriate memory variables of the household. 
- */
-int GamePlayer_retrieve_rule_details()
-{
-	//#include <string.h>
-	//To use: 	char *strcat( char *str1, const char *str2 );
-	//			char *strcpy( char *to, const char *from );
-
-	double* param_vector;
-	char*	functioncall; 
-	int	imax;
-	char* str, new_str, param_vector_as_string;
-	
-	//Rule details:
-	//The name of the rule
-	functioncall = RULEDETAILSYSTEM->array[i]->rule_execution;
-
-	//Parameters: retrieve the list of parameter values for the current rule
-	param_vector = RULEDETAILSYSTEM->array[i]->parameters;
-
-	//Convert param_vector to string
-	imax = param_vector->size;
-	str="\0"; 			//empty, null-terminated string
-	for (i=0;i<imax;i++)
-	{
-		new_str = sprintf('%s,%f',str,param_vector->array[i]);
-		strcpy(str, new_str);	
-	}
-	strcpy(param_vector_as_string,new_str);
-	
-//**********WARNING: SERIOUSLY BAD CODING BELOW******************
-	//Execute the rule's functioncall, with the appropriate parameter setting
-	sprintf('%s(%s)',functioncall, param_vector_as_string);
-
-    return 0;
-}
-
-
- /* STEP 4. Apply the selected rule.
- */
-
-/* GamePlayer_apply_rule()
+/* STEP 4. Apply the selected rule.
+ * GamePlayer_apply_rule()
  * Apply a rule.
+ * For the moment, applying a rule is nothing more than just
+ *  sending the id of the current_rule to the GameSolver.
  */
 int GamePlayer_apply_rule()
 {
-
+	GamePlayer_send_current_rule_message();
+	
     return 0;
 }
 
-
-//GamePlayer_read_ruledetailsystem_message()
-//Function to download a new rule_detail_system.
-//Used by agents to refresh their rule detail system
-//We allow for changes in:
-//parameters: changes occur due to crossover, mutation
+/* STEP 5. Change in RuleDetailSystem. 
+ * GamePlayer_read_ruledetailsystem_message()
+ * Function to check if there is a new rule_detail_system in the GameSolver agent.
+ */
 int GamePlayer_read_ruledetailsystem_message()
 {
- 	//Getting the size of the system:
-	int NR_TYPES=CLASSIFIERSYSTEM->nr_types;
-
-	//dynamic array with number of rules in each type (size of subpopulations)
-	int* NRRULES_PER_TYPE=CLASSIFIERSYSTEM->nr_rules_per_type;
-
-	//total number of rules:
-	int NRRULES=CLASSIFIERSYSTEM->nr_rules;
-	
-	//Local vars:
-	int[NRRULES] 		ids;
-	int[NRRULES]		nr_params;
-	double**			parameters;			//parameter[NRRULES][MAX_PARAMS];
-	
-	char**				rule_execution;	    //rule_execution[NRRULES]
-	int[NRRULES]		rule_type;			//rule_type[NRRULES]
-	
-	word_array * 		my_function_names = get_my_function_names(); //list of function names
-	
-	double				param_value;
-	int					i,j,k,m,jmax=0;
-
-	//HERE: As an exception, we need a local copy of ruledetailsystem
-	//because we need to copy it from the message.
-	//And then we need to copy the values using a for loop.
-	//So we need to temporarily store the contents of the message.
-	//Do we need to free memory space afterward if we make a local copy of the struct?
-	
-	RuleDetailSystem *	ruledetailsystem;
 	
 	//Reading the ruledetailsystem_message
 	ruledetailsystem_message = get_first_ruledetailsystem_message();
 	while(ruledetailsystem_message)
 	{
-		ruledetailsystem = ruledetailsystem_message->parameters;
+		//If there are new rules, there is a ruledetailsystem_message in the MB,
+		//then set new_rules to 1 in memory:
+		set_new_rules(1);
 				
 		//Proceed to next message
 		ruledetailsystem_message = get_next_ruledetailsystem_message(ruledetailsystem_message);
 	}
 	
-	for (i=0; i<NRRULES; i++)
-	{
-		//Filling the double_array parameter[i] with the parameter values of rule i
-		jmax=CLASSIFIERSYSTEM->array[i]->nr_params;
-		for (j=0; j<jmax; j++)
-		{
-			//Filling the field parameters[i][j]
-			//Problem: in xml we cannot add a name for the array[j]->(double param_value);
-			//parameters[i] is a double_array, so to access its elements we need:
-			  parameters[i][j] = ruledetailsystem->array[i]->parameters->array[j]->(double param_value);
-		}
-	}
-	
     return 0;
 }
 
-int GamePlayer_update_ruledetailsystem()
-{
-	return 0;
-}
 
 int GamePlayer_reset_private_classifiersystem()
 {
+	int new_rules = get_new_rules();
+	
  	//Getting the size of the system:
 	int NR_TYPES=CLASSIFIERSYSTEM->NR_TYPES;
 
@@ -283,17 +194,24 @@ int GamePlayer_reset_private_classifiersystem()
 	//total number of rules:
 	int NRRULES=CLASSIFIERSYSTEM->NRRULES;
 
-	//Resetting and storing values to memory:
-	CLASSIFIERSYSTEM->experience=0;
-	CLASSIFIERSYSTEM->current_rule=0;
-	CLASSIFIERSYSTEM->my_performance=0;
-
-	for (i=0; i<NRRULES; i++)
+	//Only reset if the memory variable NEWRDS==1
+	if (new_rules)
 	{
-		CLASSIFIERSYSTEM->array[i]->avgperformance=log(pow(10,-5));
-		CLASSIFIERSYSTEM->array[i]->attraction=log(pow(10,-5));
-		CLASSIFIERSYSTEM->array[i]->choiceprob=pow(10,-5);
+		//Resetting and storing values to memory:
+		CLASSIFIERSYSTEM->experience=0;
+		CLASSIFIERSYSTEM->current_rule=0;
+		CLASSIFIERSYSTEM->my_performance=0;
+	
+		for (i=0; i<NRRULES; i++)
+		{
+			CLASSIFIERSYSTEM->array[i]->avgperformance=log(pow(10,-5));
+			CLASSIFIERSYSTEM->array[i]->attraction=log(pow(10,-5));
+			CLASSIFIERSYSTEM->array[i]->choiceprob=pow(10,-5);
+		}
 	}
-
+	
+	//Reset the memory variable to 0:
+	set_new_rules(0);
+	
     return 0;
 }
--- models/AFM/afm_branches/initial/IPDgame/GameSolver_ewa_functions.c
+++ models/AFM/afm_branches/initial/IPDgame/GameSolver_ewa_functions.c
@@ -12,9 +12,10 @@
 #include "my_library_header.h"
 
 
-//GameSolver_daily_reset_public_classifiersystem
-//Daily reset of the public classifiersystem, resetting user counter, performance sum, and avgperformance.
-//This should be run before any rule_performance_messages are being read.
+//GameSolver_daily_reset_public_classifiersystem()
+//Daily reset of the public classifiersystem
+//Resets user counter, performance sum, and avgperformance.
+//This should be run before any current_rule_messages are being read.
 int GameSolver_daily_reset_public_classifiersystem()
 {
 	//total number of rules:
@@ -31,42 +32,285 @@ int GameSolver_daily_reset_public_classifiersystem()
   return 0;
 }
 
-/* GameSolver agent reads the rule_performance_message
- * - updates the rule_performance in its classifiersystem (for the selected_rule_number)
- * - responds by sending the performance measures of all the rules
+/* GameSolver_read_current_rule_message()
+ *  GameSolver agent reads the current_rule_message
  */
-int GameSolver_read_rule_performance_message()
+int GameSolver_read_current_rule_message()
 {
-	  int current_rule;
-	  double rule_performance;
+	int current_rule;
+	int agent_id;
 	
-	  rule_performance_message = get_first_rule_performance_message();
-	  while(rule_performance_message)
+	  current_rule_message = get_first_current_rule_message();
+	  while(current_rule_message)
 	  {
-	    current_rule = rule_performance_message->current_rule;
-	    rule_performance = rule_performance_message->rule_performance;
-	
-	    /* Update rule performance: */
-	    GameSolver_update_classifiersystem(current_rule, rule_performance);
-	    rule_performance_message = get_next_rule_performance_message(rule_performance_message)
+		//Read contents  
+		  agent_id     = current_rule_message->id;
+		  current_rule = current_rule_message->current_rule;
+
+	    //Store contents in memory array: automata[i]
+		  AUTOMATA->array[agent_id]->nr_rule = current_rule;
+	    
+	    //Go to next message:
+		  current_rule_message = get_next_current_rule_message(current_rule_message)
 	  }
 
    return 0;
  }
  
- 
-int GameSolver_update_classifiersystem(int current_rule, double rule_performance)
+/* GameSolver_retrieve_rule_details()
+ * Retrieve rule details
+ * After reading current_rule_messages, the GameSolver retrieves the rule details.
+ * This means copying the selected rule details to the appropriate memory variable.
+ */
+int GameSolver_retrieve_rule_details()
 {
-  
-  //Replace old performance adding new performance: ******CHECK WHEN RESET OCCURS: SHOULD BE DAILY? 
-  CLASSIFIERSYSTEM->array[current_rule]->performance += rule_performance;
-  
-  //Counter update: ******CHECK WHEN RESET OCCURS: SHOULD BE DAILY?
-  CLASSIFIERSYSTEM->array[current_rule]->counter +=1;
-  
-  //Avgperformance update:
-  CLASSIFIERSYSTEM->array[current_rule]->avgperformance = CLASSIFIERSYSTEM->array[current_rule]->avgperformance / CLASSIFIERSYSTEM->array[current_rule]->counter;
-    
+
+	return 0;
+}
+
+
+/* GameSolver_play_tournament()
+ * Runs the tournament procedure.
+ * Every submitted automaton plays a match (one 150-period iterated PD), including a match against a clone of itself.
+ * This results in an upper-diagonal matrix of results (because the game-matrix is symmetric, we do not have to run 2 times the same game,
+ * A vs. B and B vs. A). The outcome of a match is the average payoff for the automaton over the 150 periods.
+ * After all matches have been computed, we construct the full matrix of outcomes, and compute the row-average payoff for each automaton.
+ */
+int GameSolver_play_tournament()
+{
+	double 		result,sum;
+	double ** 	outcome;
+	
+	//Match [i vs. j]
+	for (i=0;i<NRAGENTS; i++)
+	{
+		for (j=0;j<i+1; j++)
+		{
+			//Automaton of agent i plays against Automaton of agent j
+			sum=0;
+			for (t=0;t<TMAX; t++)
+			{
+				//play_game is a function that should give the outcome of a game played by 2 given automata
+				result = play_game(AUTOMATON->array[i]->current_rule, AUTOMATON->array[j]->current_rule);
+				sum += result;
+			}
+			//Outcome of the match
+			outcome[i][j]=sum/TMAX;
+		}
+	}
+	//Full outcome matrix is symmetric
+	for (i=0;i<NRAGENTS; i++)
+	{
+		for (j=i;j<NRAGENTS; j++)
+		{
+			outcome[j][i]=outcome[i][j];
+		}
+	}
+	//Row-average payoffs of the symmetric outcome matrix
+	for (i=0;i<NRAGENTS; i++)
+	{
+		sum=0;
+		for (j=0;j<NRAGENTS; j++)
+		{
+			sum += outcome[i][j];
+		}
+		//Add the avg performance directly to the memory variable:
+		AUTOMATA->array[i]->rule_performance = sum/NRAGENTS;
+	}
+	
+	return 0;
+}
+
+/* int play_game(i,j)
+ * Data for 2-person game.
+ * We program how the automata play:
+ * - retrieve a copy of the automata bitstrings
+ * - read current state
+ * - travers the state block corresponding to that state
+ * - set next_move
+ * - set state transition to new_state
+ * - set payoffs fom game
+ * - return payoff of player 1
+ */
+int play_game(rule_1,rule_2)
+{
+	//Standard classification of games:
+	// |---------------|
+	// | (a,a) | (c,d) |
+	// |---------------|
+	// | (d,c) | (b,b) |
+	// |---------------|	
+	
+	//Prisoner's Dilemma: c < b <  a < d
+	int a=3, b=1, c=0, d=5;
+	int payoff[2]={0,0};
+	
+	//int arrays for bitstrings, size=148 bits
+	int* b1,b2; 	 //int arrays for bitstrings b1, b2
+	int OFFSET=4;    //offset for the start of first state_block
+	int BLOCKSIZE=9; //size of a block that encodes a state
+	int state_block[BLOCKSIZE];
+	int next_move;
+	int next_state[4];
+	int i,n,s,count;
+	
+	//Get the previous moves from memory
+	int previous_move_player0 = get(previous_move_player0);
+	int previous_move_player1 = get(previous_move_player1);
+	
+	//Retrieve a copy of the automata bitstrings:
+	b1 = RULEDETAILSYSTEM->list_of_bitstrings->array[rule_1][];
+	b2 = RULEDETAILSYSTEM->list_of_bitstrings->array[rule_2][];
+	
+	for (s=0;s<2;s++)
+	{
+		//Set the bitstring to consider
+		switch (s)
+		{
+		case 0:
+			for (i=0;i<LENGTH;i++)
+			{
+				b[i]=b1[i];
+			};
+			break;
+		case 1:
+			for (i=0;i<LENGTH;i++)
+			{
+				b[i]=b2[i];
+			};
+			break;
+		default:
+			printf("Error in GameSolver_ewa_functions.c, line 172");
+			break;
+		}
+		
+		//read current state: 4 bits	
+		//n is the current state 1-16: [0,2^n-1]
+		n=0;
+		for (i=0;i<4;i++)
+		{
+			n += pow(2,b[i]);
+		}
+		
+		//Travers the state blocks: each block is 9 bits, the first starting after the 4 bit block 
+		//States are 1-16, and we only consider the state block corresponding to current state n.
+		
+		//Enter into block n
+		count=0;
+		for (i=OFFSET+(n-1)*BLOCKSIZE; i<OFFSET+n*BLOCKSIZE; i++)
+		{
+			state_block[count] = b[i];
+			count+=1;
+		}
+		
+		//Next move:		
+		//bit 1 (index 0) of the state_block is next_move
+		next_move = state_block[0];
+		
+		//State transitions
+		//Set the next state, given the most recently observed move by opponent
+		if (s==0)
+		{
+			opponent_move = previous_move_player1;
+		}
+		if (s==1)
+		{
+			opponent_move = previous_move_player0;
+		}
+		
+		//if observed opponent move was 0, bits [2-5] (indexed 1-4) is next state
+		if (opponent_move==0)
+		{
+			count=0;
+			for (i=1;i<5; i++)
+			{
+				next_state[count]= state_block[i];
+				count+=1;
+			}
+		}
+		//if observed opponent move was 1, bits [6-9] (indexed 5-8)is next state
+		if (opponent_move==1)
+		{
+			count=0;
+			for (i=5;i<8; i++)
+			{
+				next_state[count]= state_block[i];
+				count+=1;
+			}
+		}
+		
+		//Set Move
+		switch (s)
+		{
+			case 0:
+				A0=next_move;
+				break;
+			case 1:
+				A1=next_move;
+				break;
+			default:
+				printf("Error in GameSolver_ewa_functions.c, line 172");
+				break;
+		}
+	}//End of for loop over player 1,2
+	
+	//Payoffs
+	if (A0==0 && A1==0)
+	{
+		payoff={a,a};
+	}
+	
+	if (A0==0 && A1==1)
+	{
+		payoff={c,d};
+	}
+	
+	if (A0==1 && A1==0)
+	{
+		payoff={d,c};
+	}
+	
+	if (A0==1 && A1==1)
+	{
+		payoff={b,b};
+	}
+	
+	//Set in memory the moves as previous_moves
+	set_previous_move_player0(previous_move_player0);
+	set_previous_move_player1(previous_move_player1);
+	
+	//Return only the payoff to the first player
+	return payoff[0];
+}
+
+
+/* GameSolver_update_classifiersystem()
+ * Runs the tournament procedure.
+ */
+int GameSolver_update_classifiersystem()
+{
+	int current_rule;
+	int rule_performance;
+	
+	//For each agent, retrieve the automata used, and its performance:
+	for (i=0;i<NRAGENTS;i++)
+	{
+		current_rule = AUTOMATA->array[i]->current_rule;
+		rule_performance = AUTOMATA->array[i]->rule_performance;
+		
+	  //Replace old performance adding new performance: ******CHECK WHEN RESET OCCURS: SHOULD BE DAILY? 
+	  CLASSIFIERSYSTEM->array[current_rule]->performance += rule_performance;
+	  
+	  //Counter update: ******CHECK WHEN RESET OCCURS: SHOULD BE DAILY?
+	  CLASSIFIERSYSTEM->array[current_rule]->counter +=1;
+	}
+
+	//Avgperformance update:
+	for (i=0;i<NRAGENTS;i++)
+	{	 
+	  CLASSIFIERSYSTEM->array[current_rule]->avgperformance = CLASSIFIERSYSTEM->array[current_rule]->avgperformance / CLASSIFIERSYSTEM->array[current_rule]->counter;		
+	}
+	
   return 0;
 }
 
@@ -75,7 +319,7 @@ int GameSolver_update_classifiersystem(int current_rule, double rule_performance
  */
 int GameSolver_send_all_performances_message()
 {
-  	double[] all_performances;
+  	double * all_performances;
  	 
  	//Get size of performance array:
 	imax = CLASSIFIERSYSTEM->performance->size;
@@ -92,34 +336,34 @@ int GameSolver_send_all_performances_message()
   return 0;
 }
 
-int GameSolver_update_ruledetailsystem()
-{
-	//<!--Date-event triggered: every 100 days run the GA-->
-	return 0;
-}
-
 //-Date-event triggered: every 100 days run the GA
 int GameSolver_updateGA()
 {
 	return 0;
 }
 
+int GameSolver_update_ruledetailsystem()
+{
+	//<!--Date-event triggered: every 100 days run the GA-->
+	return 0;
+}
+
+
+
 //GameSolver_send_ruledetailsystem_message()
-//Function to send ruledetailsystem updates.
+//Function sends empty ruledetailsystem_message to signal to 
+//GamePlayer agents that there has been a change in rule details.
+//(they then need to reset their private classifier to restart a learning round)
 int GameSolver_send_ruledetailsystem_message()
 {
-	//Send a message containing the entire ruledetailsystem:
-    //--> commenting the follwing line: another message sent out at the end of this fn
-	//add_ruledetailsystem_message(RULEDETAILSYSTEM, range, x, y, z);
-
-	//Alternative: we only need to send the double2D_array 
-	//RULEDETAILSYSTEM->parameters
-	double** parameters = RULEDETAILSYSTEM->parameters;
-	add_ruledetailsystem_message(parameters, range, x, y, z);
+	add_ruledetailsystem_message(range, x, y, z);
 		
     return 0;
 }
 
+/* GameSolver_reset_public_classifiersystem()
+ * Resets public classifiersystem.
+ */
 int GameSolver_reset_public_classifiersystem()
 {
 	//total number of rules:
