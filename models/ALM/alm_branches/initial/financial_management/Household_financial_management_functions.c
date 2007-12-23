7d216091763af53d87591691cb5b789995d87055
svdhoog <11057058+svdhoog@users.noreply.github.com>
2007-12-23 13:49:30 +0000

    collected all fin. management files

--- models/ALM/alm_branches/initial/financial_management/Household_financial_management_functions.c
+++ models/ALM/alm_branches/initial/financial_management/Household_financial_management_functions.c
@@ -4,213 +4,96 @@
 #include "math.h"
 
 /************Household Role: Financial Management********************************/
-/*
- * TODO:
- * 1. update code using capitalized variable names
- * 2. function names at bottom: change CamelCaps into camel_caps names.
- */
 
-
-//Household_calc_financial_management()
-int Household_compute_income_and_financial_needs()
+//int Household_financial_planning()
+int Household_financial_planning()
 {
-
-	double income_account=get_income_account();   //the running income account
-	double payment_account=get_payment_account(); //the payment account
-	double wage_income=get_wage_income();         // the wage payment from the firm
-	double capital_income= get_capital_income();  // the capital gain on the asset portfolio
-	double gross_income=0.0;                      //Total gross income: labour income, capital income
-	double tax_provision=0.0;
-	double leftover_consumption_budget=get_leftover_consumption_budget(); //Left-over budget from previous month (read from memory)
-	double net_income=0.0;                        //Total net income for this month
-	
-	double savings_account_diff=0.0;              //Savings Account differential: addition
-	double savings_account=get_savings_account();
-	
-	double total_budget=0.0;
-	double asset_wealth=0.0;                //Total asset value evaluated at current market prices
-	double cash_on_hands=0.0;                //Value of all liquid assets: payment_account, savings_account, asset_portfolio
-	
-	double consumption_ratio=get_consumption_ratio();
-	double total_consumption_budget=0.0;    //Total fraction of cash_on_hands that is allocated to consumption expenditure this month
-	double weekly_consumption_budget=0.0;   //Transformation of the total_consumption_budget to weekly budget
-	double financial_needs=0.0;             //Total financial needs that are not covered by the cash_on_hands: 
-	double financial_surplus=0.0;           //Financial surplus
-	double add_financial_needs=0.0;
-	
-	double current_debt=get_current_debt();
-	double target_debt=0.0;
-	double interest_payment=0.0;            //Interest payments on outstanding debts
-	double debt_repayment=0.0;              //Value of debt repayments (equal to current_debts?)
-	double loan_application=0.0;            //Value of new loans to obtain with the bank
-	
-	double asset_ratio=get_asset_ratio();
-	double asset_budget=0.0;                //Budget allocation for expenditure on the asset market
-	
-	
 	//Total gross income: labour income and capital income
-	gross_income = wage_income + capital_income;
+	GROSS_INCOME = WAGE_INCOME + CAPITAL_INCOME;
 	
 	//Compute taxes
-	tax_provision = income_tax_rate*gross_income;
+	TAX_PAYMENT = INCOME_TAX_RATE*GROSS_INCOME;
+	
+	//Send tax to goverment
+	add_tax_payment_message(ID, gov_id, TAX_PAYMENT, MSGDATA);
 	
 	//Compute net income
-	net_income = (1-income_tax_rate)*gross_income+leftover_consumption_budget;
+	INCOME_ACCOUNT = (1-INCOME_TAX_RATE)*GROSS_INCOME + LEFTOVER_CONSUMPTION_BUDGET;
 	
-	//Additional savings from net income
-	savings_account_diff=0; //if no savings
-	/* C-CODE */
-	// savings_account_diff= ; //if some savings
+	//If no savings
+	SAVINGS_PROPENSITY =0;
 	
+	//Savings from net income
+	SAVINGS = SAVINGS_PROPENSITY * INCOME_ACCOUNT;	
 	
-	savings_account += savings; 
+	SAVINGS_ACCOUNT += SAVINGS; 
 	
-	//Total budget
-	total_budget=net_income-savings;
+	//Total budget: transfer funds from income account to payment account, minus savings
+	PAYMENT_ACCOUNT = INCOME_ACCOUNT - SAVINGS;
+	
+	//Reset the income account
+	INCOME_ACCOUNT =0;
 	
 	//Current asset wealth, evaluated at current market prices
 	/* C-CODE */
-	// asset_wealth =
+	// ASSET_WEALTH =
 	
 	//Cash-on-hands
-	cash_on_hands = total_budget + asset_wealth;
+	CASH_ON_HANDS = PAYMENT_ACCOUNT + ASSET_WEALTH;
 	
 	//Consumption budget
-	total_consumption_budget=consumption_ratio*total_budget;
-	weekly_consumption_budget=total_consumption_budget/4;
+	if(CASH_ON_HANDS > MEAN_INCOME)
+	{
+		
+		TOTAL_CONSUMPTION_BUDGET = CASH_ON_HANDS + EXCESS_CONSUMPTION_PROPENSITY*(CASH_ON_HANDS - MEAN_INCOME);
+	}
+	else
+	{
+		TOTAL_CONSUMPTION_BUDGET = CASH_ON_HANDS;
+	}
 	
-	//Financial needs/surplusses
-	financial_needs = max(0, total_consumption_budget-cash_on_hands)
-	financial_surplus = max(0, cash_on_hands-total_consumption_budget);
+	//Weekly consumption budget
+	WEEKLY_CONSUMPTION_BUDGET = TOTAL_CONSUMPTION_BUDGET/4;
+	
+	//Financial needs and asset_budget
+	FINANCIAL_NEEDS = 0; FINANCIAL_SURPLUS=0;
+	if (TOTAL_CONSUMPTION_BUDGET > CASH_ON_HANDS)
+	{
+		//planned consumption superceeds cash_on_hands: sell assets
+		FINANCIAL_NEEDS = TOTAL_CONSUMPTION_BUDGET - CASH_ON_HANDS;
+		ASSET_BUDGET = -FINANCIAL_NEEDS;
+	}
+	else
+		{
+			//planned consumption is less than cash_on_hands: use the excess to buy assets
+			FINANCIAL_SURPLUS = max(0, CASH_ON_HANDS-TOTAL_CONSUMPTION_BUDGET);
+			ASSET_BUDGET = FINANCIAL_SURPLUS;
+		}
+
+	
 	
 	/******************************* NO HOUSEHOLD CREDIT YET ************************************/
 	//Interest payments on outstanding debts
-	//interest_payments=interest*current_debt;
+	//INTEREST_PAYMENTS=INTEREST*CURRENT_DEBT;
 	
 	//Interest payment message to bank
-	//add_interest_payment_message(household_id, bank_id, interest_payments, range, x, y, z);
-	//payment_account -= interest_payments;
+	//add_interest_payment_message(household_id, bank_id, INTEREST_PAYMENTS, MSGDATA);
+	//PAYMENT_ACCOUNT -= INTEREST_PAYMENTS;
 	
 	//Debt repayments
-	//debt_repayments=current_debt;
+	//DEBT_REPAYMENTS=CURRENT_DEBT;
 	
 	//Debt repayment message to bank:
-	//add_debt_repayment_message(household_id, bank_id, debt_repayments, range, x, y, z);
-	//payment_account -= debt_repayments;
+	//add_debt_repayment_message(household_id, bank_id, DEBT_REPAYMENTS, MSGDATA);
+	//PAYMENT_ACCOUNT -= DEBT_REPAYMENTS;
 	
 	//New loans
-	//loan_application=min(financial_needs, max_debt);
+	//loan_application=min(FINANCIAL_NEEDS, MAX_DEBT);
 	
-	//Additional financial needs
-	//add_financial_needs = max(0,financial_needs-loan_application);
+	//Financial needs
+	//EXTERNAL_FINANCIAL_NEEDS = max(0,FINANCIAL_NEEDS-LOAN_APPLICATION);
 	
 	/********************************************************************************************/
 	
-	
-	//Allocation of funds to the asset market
-	asset_budget=asset_ratio*total_budget;
-	
-	
-	set_income_account(income_account);
-	set_payment_account(payment_account);
-	set_gross_income(gross_income);
-	set_tax_payment(tax_payment);
-	set_net_income(net_income);
-	set_savings_account(savings_account);
-	set_total_budget(total_budget);
-	set_asset_wealth(asset_wealth);
-	set_cash_on_hands(cash_on_hands);
-	set_total_consumption_budget(total_consumption_budget);
-	set_weekly_consumption_budget(weekly_consumption_budget);
-	set_financial_needs(financial_needs);
-	
-	set_asset_budget(asset_budget);
-	set_target_asset_portfolio(target_asset_portfolio);
-/*
-	set_target_debt(target_debt);
-	set_interest_payment(interest_payment);
-	set_debt_repayment(debt_repayment);
-	set_loan_application(loan_application);
-*/
-    return 0;
-}
-
-
-int HouseholdCalculateMonthlyTaxes()
-{
-    return 0;
-}
-
-int HouseholdCalculateNetIncome()
-{
-    return 0;
-}
-
-
-int HouseholdCalculateTotalBudget()
-{
-    return 0;
-}
-
-int HouseholdCalculateAssetWealth()
-{
-    return 0;
-}
-
-
-int HouseholdCalculateCashOnHands()
-{
-    return 0;
-}
-
-
-int HouseholdCalculateConsumptionBudget()
-{
-    return 0;
-}
-
-int HouseholdCalculateFinancialNeedsMonthly()
-{
-    return 0;
-}
-
-/// ConsumptionMarket role
-///asset market role
-
-int HouseholdUpdateAssetPortfolio()
-{
-    return 0;
-}
-
-
-int HouseholdCalculateFinancialNeedsDaily()
-{
-    return 0;
-}
-
-
-int HouseholdEntryDecision()
-{
-    return 0;
-}
-
-int HouseholdCalculateAssetBudget()
-{
-    return 0;
-}
-
-
-int HouseholdCalculateFirmBondOrders()
-{
-    return 0;
-}
-
-int HouseholdCalculateGovernmentBondOrders()
-{
-    return 0;
-}
-
-int HouseholdCalculateFirmStockOrders()
-{
     return 0;
 }
