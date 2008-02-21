7552c93d7cce2945a720ac1944aa16a12dba0eed
sysfal <sysfal@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-02-21 10:02:57 +0000

    Addition of Clearinghouse functions

---
+++ models/ALM/alm_branches/simon/financial_management/Clearinghouse_financial_management_functions.c
@@ -0,0 +1,18 @@
+#include "header.h"
+#include "Clearinghouse_agent_header.h"
+#include "my_library_header.h"
+
+int Clearinghouse_read_order_messages()
+{
+	return 0;
+}
+
+int Clearinghouse_compute_transactions()
+{
+	return 0;
+}
+
+int Clearinghouse_send_transaction_message()
+{
+	return 0;
+}
