34064b948886c603b964f678e1a99b92323bbb7a
sysfal <sysfal@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-02-26 10:47:06 +0000

    Addition of my library functions files

---
+++ models/ALM/alm_branches/simon/financial_management/my_library_header.h
@@ -0,0 +1,25 @@
+/**
+ * \file  my_library_header.h
+ * \brief Header for user created library functions.
+ */
+
+/** \def DAY
+ * \brief The day count. */
+#define DAY iteration_loop
+
+/** \def WEEK
+ * \brief Number of days in a week. */
+#define WEEK 5
+/** \def MONTH
+ * \brief Number of days in a month. */
+#define MONTH 20
+/** \def QUARTER
+ * \brief The number of days in a quarter. */
+#define QUARTER 60
+/** \def MSGDATA
+ * \brief The additional data that messages need when sent. */
+#define MSGDATA 1.0, 0.0, 0.0, 0.0
+/*#define MSGDATA get_range(), get_posx(), get_posy(), get_posz()*/
+/** \def PRINT_LOG
+ * \brief A flag to print log to terminal. */
+#define PRINT_LOG 1
