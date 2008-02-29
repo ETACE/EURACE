1067db43846bf47b346acaaae2cee5f7c16f93fc
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-02-29 11:53:20 +0000

    AFM/afm_branches/initial/ewa_learning: - model-ewa.xml: added SimplePublicClassifierSystem and SimplePrivateClassifierSystem - changed -> to . notation for structs - added unit tests

---
+++ models/AFM/afm_branches/initial/ewa_learning/Unittest/Household_ewa_unittest.c
@@ -0,0 +1,47 @@
+#include <CUnit/Basic.h>
+#include "header.h"
+#include "Household_agent_header.h"
+#include "my_library_header.h"
+
+/************Household: EWA learning Role ********************************/
+
+/************ Unit tests ********************************/
+void unittest_Household_send_rule_performance()
+{
+
+}
+
+void unittest_Household_read_all_performances()
+{
+
+}
+
+void unittest_Household_select_rule()
+{
+
+}
+
+void unittest_Household_retrieve_rule_details()
+{
+
+}
+
+void unittest_Household_apply_rule()
+{
+
+}
+
+void unittest_Household_read_transaction()
+{
+
+}
+
+void unittest_Household_read_and_update_ruledetailsystem()
+{
+
+}
+
+void unittest_Household_reset_private_classifiersystem()
+{
+
+}
