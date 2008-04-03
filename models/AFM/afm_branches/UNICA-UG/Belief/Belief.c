704209c7ffc32f2f8007a106ac0c2b22aea7200c
mario <mario@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-04-03 13:47:37 +0000

    update belief

--- models/AFM/afm_branches/UNICA-UG/Belief/Belief.c
+++ models/AFM/afm_branches/UNICA-UG/Belief/Belief.c
@@ -1,26 +1,27 @@
 #include "Belief.h"
 
 Belief *newBelief()
-{ Belief *temp;
+{ 
+  Belief *temp;
   temp=(Belief *)malloc(sizeof(Belief));
-   initializeBelief(temp);
+  initializeBelief(temp);
   return temp;
  
 }
 
 void initializeBelief(Belief *belief)
 {
-                      belief->assetsExpectedPriceReturns=0.0;
-       belief->assetsExpectedTotalReturns=0.0;
-      belief->assetsExpectedCashFlowYield=0.0;
-      belief->volatility=0.0;
-      belief->forwardWindow=0;
-      belief->backwordWindow=0;
-      belief->binsNumber=0;
-       belief->randomReturnWeigth=0.0;
-       belief->fundametalReturnWeigth=0.0;
-       belief->chartistReturnWeigth=0.0;
-      belief->holdingPeriodToForwardW=0;
+      belief->expectedPriceReturns=0.0;
+      belief->expectedTotalReturns=0.0;
+      belief->expectedCashFlowYield=0.0;
+      //belief->volatility=0.0;
+      //belief->forwardWindow=0;
+      //belief->backwordWindow=0;
+      //belief->binsNumber=0;
+      //belief->randomReturnWeigth=0.0;
+      //belief->fundametalReturnWeigth=0.0;
+      //belief->chartistReturnWeigth=0.0;
+      //belief->holdingPeriodToForwardW=0;
 }
 
 void  assetBeliefFormation(Belief *belief, CAsset *assets,Random *rnd)
@@ -37,4 +38,49 @@ double stockExpectedPriceReturns(Belief *belief,Asset *anAsset)
       
        expectedPriceReturn=belief->assetsExpectedPriceReturns;
        return expectedPriceReturn;
+}
+
+int forwardMonths(Belief *belief, int currentDay)
+{   int forwardWindow;
+    int nrForwardMonths;
+    forwardWindow=belief->forwardWindow;
+    nrForwardMonths=quo(currentDay+forwardWindow,dayInMonth)-quo(forwardWindow,dayInMonth);
+    return nrForwardMonths;
+}
+    
+double horizonRetainedEarnings(Belief *belief,Stock *stock, int currentDay)
+{
+       int forwardmonths;
+       double hRetainedEarnings;
+       forwardmonths=forwardMonths(belief,currentDay);
+       hRetainedEarnings= forwardMonths*(stock->earningsExp-stock->earningsPayoutExp);  
+       return hRetainedEarnings;
+}
+
+double futureFundamentalReturn(Belief *belief,Stock *stock,int currentDay)
+{ double futureFundPrice;
+  double fundamentalReturn;
+  hRetainedEarnings=horizonRetainedEarnings(belief,stock,currentDay);
+  futureFundlPrice = (stock->equity+hRetainedEarnings)/stock->nrOutStandingShares;
+  fundamentalReturn = (FutureFundamentalPrice-lastMarketPrice(stock))/lastMarketPrice(stock);
+  return fundamentalReturn;
+       }
+
+double randomReturn(Belief *belief, Stock *stock,int forwardWindow, Random *rnd)
+{  double rndReturn;
+   double volatility;
+   double randn;
+   volatility=stock->volatility;
+   randn=gauss(rnd,0,1);
+   rndRetun = sqrt(forwardWindow)*volatility*randn;
+   return rndReturn;
+}
+void  stockBeliefFormation(Belief *belief, Stock *stock,int backwardWindow, Random *rnd)
+{ double fundamentalReturn;
+  double rndReturn;
+  CDouble *historicalReturn;
+  historicalReturn=newCDouble(historicalReturn,backwardWindow-1);
+  //prendo una finestra computo i ritorni da fare...
+  
+  
 }
