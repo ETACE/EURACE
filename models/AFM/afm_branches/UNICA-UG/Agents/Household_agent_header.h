72aa1ed3a303b6f0b644ff5f8b5c25527fae2a0f
mario <mario@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-04-07 18:55:59 +0000

    inserito file sh

--- models/AFM/afm_branches/UNICA-UG/Agents/Household_agent_header.h
+++ models/AFM/afm_branches/UNICA-UG/Agents/Household_agent_header.h
@@ -21,6 +21,30 @@
 /** \def ASSETSOWNED
  * \brief Direct access to assetsowned of Household agent memory variable. */
 #define ASSETSOWNED current_xmachine->xmachine_Household->assetsowned
+/** \def FORWARDWINDOW
+ * \brief Direct access to forwardWindow of Household agent memory variable. */
+#define FORWARDWINDOW current_xmachine->xmachine_Household->forwardWindow
+/** \def BACKWORDWINDOW
+ * \brief Direct access to backwordWindow of Household agent memory variable. */
+#define BACKWORDWINDOW current_xmachine->xmachine_Household->backwordWindow
+/** \def BINSNUMBER
+ * \brief Direct access to binsNumber of Household agent memory variable. */
+#define BINSNUMBER current_xmachine->xmachine_Household->binsNumber
+/** \def RANDOMRETURNWEIGTH
+ * \brief Direct access to randomReturnWeigth of Household agent memory variable. */
+#define RANDOMRETURNWEIGTH current_xmachine->xmachine_Household->randomReturnWeigth
+/** \def FUNDAMETALRETURNWEIGTH
+ * \brief Direct access to fundametalReturnWeigth of Household agent memory variable. */
+#define FUNDAMETALRETURNWEIGTH current_xmachine->xmachine_Household->fundametalReturnWeigth
+/** \def CHARTISTRETURNWEIGTH
+ * \brief Direct access to chartistReturnWeigth of Household agent memory variable. */
+#define CHARTISTRETURNWEIGTH current_xmachine->xmachine_Household->chartistReturnWeigth
+/** \def HOLDINGPERIODTOFORWARDW
+ * \brief Direct access to holdingPeriodToForwardW of Household agent memory variable. */
+#define HOLDINGPERIODTOFORWARDW current_xmachine->xmachine_Household->holdingPeriodToForwardW
+/** \def RANGE
+ * \brief Direct access to range of Household agent memory variable. */
+#define RANGE current_xmachine->xmachine_Household->range
 /** \def POSX
  * \brief Direct access to posx of Household agent memory variable. */
 #define POSX current_xmachine->xmachine_Household->posx
