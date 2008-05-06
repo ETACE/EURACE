e3a78f7d992d93375ac111aaa0b75400999b6c5b
Simon Coakley <869932+sysfal@users.noreply.github.com>
2008-05-06 09:43:38 +0000

    update micro version

--- FLAME/branches/simon/xparser_0.15/header.h
+++ FLAME/branches/simon/xparser_0.15/header.h
@@ -13,7 +13,7 @@
 #define VERSIONMINOR 15
 /** \def VERSIONMICRO
  * \brief Bug fixes. */
-#define VERSIONMICRO 0
+#define VERSIONMICRO 1
 /** \def DEBUG
  * \brief Debug output. */
 #define DEBUG 0
