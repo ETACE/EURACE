9b6e27d37d3c182722b37655d153788c53e7a4e1
mariam <mariam@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-05-07 10:00:42 +0000

    new changes by mariam and simon G

---
+++ models/EURACE_Model/Credit_UNIBI/Firm_functions.c
@@ -0,0 +1,24 @@
+#include "header.h"
+#include "Firm_agent_header.h"
+
+
+int Firm_savings()
+{ 
+	return 0;
+}
+
+
+int Firm_ask_for_loan()
+{
+	return 0;
+}
+
+int Firm_choose_bank()
+{
+	return 0;
+}
+
+int Firm_pay_debt()
+{
+	return 0;
+}
---
+++ models/EURACE_Model/Credit_UNIBI/HH_functions.c
@@ -0,0 +1,8 @@
+#include "header.h"
+#include "Firm_agent_header.h"
+
+
+int Household_savings_decision()
+{ 
+	return 0;
+}
