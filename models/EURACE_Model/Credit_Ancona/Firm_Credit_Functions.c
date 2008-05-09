fd0333bffe7375aae4785cb2a955569e078b05cd
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-05-09 09:40:22 +0000

    edits Credit_Ancona

--- models/EURACE_Model/Credit_Ancona/Firm_Credit_Functions.c
+++ models/EURACE_Model/Credit_Ancona/Firm_Credit_Functions.c
@@ -2,6 +2,7 @@
 #include "Firm_agent_header.h"
 #include "my_library_header.h"
 
+#define NUMBER_OF_BANKS 10
 
 int Firm_ask_loan()
 {
@@ -14,15 +15,16 @@ int Firm_ask_loan()
 	    DMARKETMATRIX.array[j]=0;
 	} 
 	
-	/*WARNING: CREDIT_DEMAND is also an agent memory variable*/
-	
       while (connected<LINK)
       {
 
             j=(int)((rand()/RAND_MAX)*NUMBER_OF_BANKS);// choose 'LINK' banks
             DMARKETMATRIX.array[j]=1;
-            D_LOAN = 100*(rand()/RAND_MAX);  /* generate random credit demand */
-            add_loan_request_message(ASSET, TOTAL_DEBT, D_LOAN, ID, j);
+            
+            //old code: generate random credit demand 
+            //D_LOAN = 100*(rand()/RAND_MAX);
+            
+            add_loan_request_message(ID, j, EQUITY, TOTAL_DEBT, EXTERNAL_FINANCIAL_NEEDS);
             connected+=1;
       }  
       
@@ -32,7 +34,7 @@ int Firm_ask_loan()
 int Firm_get_loan()
 {
 	int n, n1, k, i, j, primo;
-      int bk = -1;
+    int bk = -1;
 	double interest, aux;
 	double totalcredit_taken=0;
 
@@ -53,7 +55,7 @@ int Firm_get_loan()
 	START_LOAN_CONDITIONS_MESSAGE_LOOP
 	if (ID == loan_conditions_message->firm_id) 
 	{
-            bk = loan_conditions_message->bank_id;
+        bk = loan_conditions_message->bank_id;
         INTEREST.array[bk] = loan_conditions_message->proposed_interest_rate;
 		CREDIT_OFFER.array[bk] = loan_conditions_message->amount_offered_credit;
 		RATEORDER.array[bk] = loan_conditions_message->bank_id;
@@ -62,113 +64,77 @@ int Firm_get_loan()
 	}
 	FINISH_LOAN_CONDITIONS_MESSAGE_LOOP
        
-      n1=0;
+    n1=0;
 
 	for(i=0;i<NUMBER_OF_BANKS-1;i++)
-      {
+    {
             for(k=i+1; k<NUMBER_OF_BANKS; k++)
             {
-      	    if (INTEREST.array[i]>INTEREST.array[k]) 
-	          {
-		       aux=INTEREST.array[i];
- 			 INTEREST.array[i]=INTEREST.array[k];
- 			 INTEREST.array[k]=aux;
- 			 n1=RATEORDER.array[i];
- 			 RATEORDER.array[i]=RATEORDER.array[k];
- 			 RATEORDER.array[k]=n1;
+            	if (INTEREST.array[i]>INTEREST.array[k]) 
+            	{
+			     aux=INTEREST.array[i];
+	 			 INTEREST.array[i]=INTEREST.array[k];
+	 			 INTEREST.array[k]=aux;
+	 			 n1=RATEORDER.array[i];
+	 			 RATEORDER.array[i]=RATEORDER.array[k];
+	 			 RATEORDER.array[k]=n1;
                 }
             }
-      }
+    }
 	
-      for(primo=0; primo<NUMBER_OF_BANKS; primo++)
-      {
-         if (DMARKETMATRIX.array[RATEORDER.array[primo]]==1)
-         {
-            credit_accepted = D_LOAN - totalcredit_taken;
-            if (credit_accepted>=CREDIT_OFFER.array[RATEORDER.array[primo]])
-            {
-               credit_accepted=CREDIT_OFFER.array[RATEORDER.array[primo]];
-            }
-
-            totalcredit_taken += credit_accepted;
-            interest = credit_accepted*INTEREST.array[RATEORDER.array[primo]];
-            
-            //Old code, now merged
-            //OUTSTANDING_DEBT.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1] = credit_accepted+interest;		          
-            //INSTALMENT_AMOUNT.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1] = (credit_accepted+interest)/INSTALMENT_NUMBER;
-            //INTEREST_AMOUNT.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1] = interest/INSTALMENT_NUMBER;
-            //RESIDUAL_VAR.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1] = VALUE_AT_RISK.array[RATEORDER.array[primo]]*(CREDIT_OFFER.array[RATEORDER.array[primo]]/credit_accepted);
-            //VAR_PER_INSTALMENT.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1] =RESIDUAL_VAR.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1]/INSTALMENT_NUMBER;
-            
-            bank_id = RATEORDER.array[primo];
-            loan_value = credit_accepted;
-            interest_rate = INTEREST.array[RATEORDER.array[primo]];
-            installment_amount = (credit_accepted+interest)/INSTALMENT_NUMBER;
-            var_per_installment = RESIDUAL_VAR.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1]/INSTALMENT_NUMBER;
-            residual_var = VALUE_AT_RISK.array[RATEORDER.array[primo]]*(CREDIT_OFFER.array[RATEORDER.array[primo]]/credit_accepted);
-            bad_debt = 0.0;
-            nr_periods_before_repayment=INSTALMENT_NUMBER;
-            	
-            add_debt_item(&LOANS, bank_id, loan_value, interest_rate, installment_amount, var_per_installment, residual_var, bad_debt, nr_periods_before_repayment);
-            add_loan_acceptance_message(bank_id, credit_accepted);
-
-            PAYMENT_ACCOUNT += credit_accepted;
-         }
-      }
-
-
-      return 0;
-}
-
-// !!!! This functions needs still to be merged!!!!!
-int Firm_pay_interest_instalment()
-{
-	double c, r, v, bad;
-	int a, i, j, k;
-	double total_instalments = 0;
-	double individual_var;
-	double individual_debt;
+	  for(primo=0; primo<NUMBER_OF_BANKS; primo++)
+	  {
+	     if (DMARKETMATRIX.array[RATEORDER.array[primo]]==1)
+	     {
+	        credit_accepted = D_LOAN - totalcredit_taken;
+	        if (credit_accepted>=CREDIT_OFFER.array[RATEORDER.array[primo]])
+	        {
+	           credit_accepted=CREDIT_OFFER.array[RATEORDER.array[primo]];
+	        }
 	
-      TOTAL_DEBT=0;
-
-	for ( i = 0; i < NUMBER_OF_BANKS; i++ ) 
-	{
-            for (j=0; j<INSTALMENT_NUMBER; j++)
-            {
-                total_instalments+=(INSTALMENT_AMOUNT.array[i][j]+INTEREST_AMOUNT.array[i][j]);
-                TOTAL_DEBT += OUTSTANDING_DEBT.array[i][j];
-            }
-
+	        totalcredit_taken += credit_accepted;
+	        interest = credit_accepted*INTEREST.array[RATEORDER.array[primo]];
+	        
+		    //Old code, now merged
+		    //OUTSTANDING_DEBT.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1] = credit_accepted+interest;		          
+		    //INSTALMENT_AMOUNT.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1] = (credit_accepted+interest)/INSTALMENT_NUMBER;
+		    //INTEREST_AMOUNT.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1] = interest/INSTALMENT_NUMBER;
+		    //RESIDUAL_VAR.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1] = VALUE_AT_RISK.array[RATEORDER.array[primo]]*(CREDIT_OFFER.array[RATEORDER.array[primo]]/credit_accepted);
+		    //VAR_PER_INSTALMENT.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1] =RESIDUAL_VAR.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1]/INSTALMENT_NUMBER;
+		    
+		    bank_id = RATEORDER.array[primo];
+		    loan_value = credit_accepted;
+		    interest_rate = INTEREST.array[RATEORDER.array[primo]];
+		    installment_amount = (credit_accepted+interest)/INSTALMENT_NUMBER;
+		    var_per_installment = RESIDUAL_VAR.array[RATEORDER.array[primo]][INSTALMENT_NUMBER-1]/INSTALMENT_NUMBER;
+		    residual_var = VALUE_AT_RISK.array[RATEORDER.array[primo]]*(CREDIT_OFFER.array[RATEORDER.array[primo]]/credit_accepted);
+		    bad_debt = 0.0;
+		    nr_periods_before_repayment=INSTALMENT_NUMBER;
+		    	
+		    add_debt_item(&LOANS, bank_id, loan_value, interest_rate, installment_amount, var_per_installment, residual_var, bad_debt, nr_periods_before_repayment);
+		    add_loan_acceptance_message(bank_id, credit_accepted);
+		
+		    //update the payment_account with the amount of credit obtained
+		    PAYMENT_ACCOUNT += credit_accepted;
+	     }
 	}
 
+	  return 0;
+}
+
+/*
+// This functions is no longer needed, the debt installments are splitted into 2 messages:
+// installment_message() and bankruptcy_message()
+int Firm_pay_interest_instalment()
+{
+	int i;
 
 	if ( TOTAL_RESOURCES >= total_instalments ) 
 	{
-		for ( i = 0; i < NUMBER_OF_BANKS; i++ ) 
-            {
-                for (j=0; j<INSTALMENT_NUMBER; j++)
-                {
-			  c = INSTALMENT_AMOUNT.array[i][j];
-			  r = INTEREST_AMOUNT.array[i][j];
-			  v = VAR_PER_INSTALMENT.array[i][j];
-			  TOTAL_RESOURCES -= (c+r);
-			  OUTSTANDING_DEBT.array[i][j] -= c;
-			  INTEREST_LEFT.array[i] -= r;
-			  RESIDUAL_VAR.array[i] -= v;
-			  ASSET-=r; 
-                    bad=0; 
-                    a=0; 
-                    individual_var=0; 
-                    add_instalment_message(c, r, i, v,bad, a, individual_var);
-		    }
-
-	          for (k=1; k<INSTALMENT_NUMBER; k++)//sposta di un periodo
-                {       
-                    OUTSTANDING_DEBT.array[i][k-1]=OUTSTANDING_DEBT.array[i][k]; 
-                    VAR_PER_INSTALMENT.array[i][k-1]=VAR_PER_INSTALMENT.array[i][k]; 
-                    INTEREST_AMOUNT.array[i][k-1]=INTEREST_AMOUNT.array[i][k];
-                }
-            }
+		for (i = 0; i < LOANS.size; i++ ) 
+        {
+              add_installment_message(LOANS.array[i].bank_id, LOANS.array[i].installment_amount, LOANS.array[i].interest_amount, LOANS.array[i].var_per_installment)
+         }
       }				
 			
 
@@ -197,5 +163,4 @@ int Firm_pay_interest_instalment()
 
 	return 0;
 }
-
-
+*/
