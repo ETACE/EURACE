7f1c0c2edfbbfc1e5d4eeeb521e451bc59d7a0cf
Simon Coakley <869932+sysfal@users.noreply.github.com>
2008-05-09 09:06:38 +0000

    increment version micro number

--- FLAME/branches/simon/xparser_0.15/header.h
+++ FLAME/branches/simon/xparser_0.15/header.h
@@ -13,7 +13,7 @@
 #define VERSIONMINOR 15
 /** \def VERSIONMICRO
  * \brief Bug fixes. */
-#define VERSIONMICRO 5
+#define VERSIONMICRO 6
 /** \def DEBUG
  * \brief Debug output. */
 #define DEBUG 0
