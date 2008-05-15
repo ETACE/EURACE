a252888f9fbdd05bf4782a447de55e545f4904ac
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-05-15 15:18:09 +0000

    added <Agent>_read_tax_rates msg for Firm, IGFirm, HH

--- models/EURACE_Model/trunk/Statistical_Office_UNIBI/Firm_Statistics_Functions.c
+++ models/EURACE_Model/trunk/Statistical_Office_UNIBI/Firm_Statistics_Functions.c
@@ -22,6 +22,22 @@ int Firm_send_payments_to_bank()
 	return 0;
 }
 
+/** \fn Firm_read_tax_rates()
+ * \brief Firms reads the tax_rates_messages from Governments
+ */
+int Firm_read_tax_rates()
+{
+	//Message send by Government:
+	START_GOVERNMENT_TAX_RATES_MESSAGE_LOOP			
+		if(government_tax_rates_message->gov_id == GOV_ID)
+		{
+			TAX_RATE_CORPORATE = government_tax_rates_message->tax_rate_corporate;
+			TAX_RATE_VAT = government_tax_rates_message->tax_rate_vat;
+		}
+	FINISH_GOVERNMENT_TAX_RATES_MESSAGE_LOOP
+
+	return 0;
+}
 
 /** \fn Firm_receive_data()
  * \brief Firms receive the data messages from the Eurostat*/
@@ -68,17 +84,6 @@ int Firm_receive_data()
 			}
 		}
 		FINISH_EUROSTAT_SEND_SPECIFIC_SKILLS_MESSAGE_LOOP
-
-		//Message send by Government:
-		START_GOVERNMENT_TAX_RATES_MESSAGE_LOOP			
-			if(government_tax_rates_message->gov_id == GOV_ID)
-			{
-				TAX_RATE_CORPORATE = government_tax_rates_message->tax_rate_corporate;
-				TAX_RATE_HH_LABOUR = government_tax_rates_message->tax_rate_hh_labour;
-				TAX_RATE_HH_CAPITAL = government_tax_rates_message->tax_rate_hh_capital;
-				TAX_RATE_VAT = government_tax_rates_message->tax_rate_vat;
-			}
-		FINISH_GOVERNMENT_TAX_RATES_MESSAGE_LOOP
 
 		return 0;
 }
--- models/EURACE_Model/trunk/Statistical_Office_UNIBI/Household_Statistics_Functions.c
+++ models/EURACE_Model/trunk/Statistical_Office_UNIBI/Household_Statistics_Functions.c
@@ -7,21 +7,19 @@
 /************************************ Household agent functions ************************************/
 /*************************************Household Role: Statistics *********************************/
 
-/** \fn Household_receive_data()
- * \brief Household receives data messages from Eurostat*/
-int Household_receive_data()
+/** \fn Household_read_tax_rates()
+ * \brief Household reads the tax_rates_messages from Governments
+ */
+int Household_read_tax_rates()
 {
 	//Message send by Government:
-		START_GOVERNMENT_TAX_RATES_MESSAGE_LOOP			
-			if(government_tax_rates_message->gov_id == GOV_ID)
-			{
-				TAX_RATE_CORPORATE = government_tax_rates_message->tax_rate_corporate;
-				TAX_RATE_HH_LABOUR = government_tax_rates_message->tax_rate_hh_labour;
-				TAX_RATE_HH_CAPITAL = government_tax_rates_message->tax_rate_hh_capital;
-				TAX_RATE_VAT = government_tax_rates_message->tax_rate_vat;
-			}
-		FINISH_GOVERNMENT_TAX_RATES_MESSAGE_LOOP
+	START_GOVERNMENT_TAX_RATES_MESSAGE_LOOP			
+		if(government_tax_rates_message->gov_id == GOV_ID)
+		{
+			TAX_RATE_HH_LABOUR = government_tax_rates_message->tax_rate_hh_labour;
+			TAX_RATE_HH_CAPITAL = government_tax_rates_message->tax_rate_hh_capital;
+		}
+	FINISH_GOVERNMENT_TAX_RATES_MESSAGE_LOOP
 
-
-		return 0;
+	return 0;
 }
