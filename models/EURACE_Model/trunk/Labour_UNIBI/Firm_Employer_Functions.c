880ebf087642a81e7b794da743305437d79eedc5
svdhoog <11057058+svdhoog@users.noreply.github.com>
2008-05-15 08:59:38 +0000

    New functions: - Government_UG/Government_send_tax_rates - Statistical_Office_UNIBI/Eurostat_Functions.c: Eurostat_read_tax_rates - Statistical_Office_UNIBI/Household_Statistics_Functions.c: Household_receive_data - Labour_UNIBI/Firm_Employer_Functions.c: Firm_receive_data
    New tax rate messages:
    - Government sends government_tax_rates_message to Eurostat
    - Eurostat sends eurostat_tax_rates_message to Household, Firm
    - HH reads government_tax_rates_message and eurostat_tax_rates_message
    - Firm reads government_tax_rates_message and eurostat_tax_rates_message

--- models/EURACE_Model/trunk/Labour_UNIBI/Firm_Employer_Functions.c
+++ models/EURACE_Model/trunk/Labour_UNIBI/Firm_Employer_Functions.c
@@ -88,11 +88,36 @@ int Firm_receive_data()
 				specific_skill_5;
 			}
 		}
-
 		FINISH_EUROSTAT_SEND_SPECIFIC_SKILLS_MESSAGE_LOOP
 
-		return 0;
+		//Message send by Eurostat:
+/*
+		START_EUROSTAT_TAX_RATES_MESSAGE_LOOP
+		for (i=0; i<eurostat_tax_rates_message->government_tax_rates.size; i++)
+		{
+			if(eurostat_tax_rates_message->government_tax_rates.array[i].gov_id == GOV_ID)
+			{
+				TAX_RATE_CORPORATE = eurostat_tax_rates_message->government_tax_rates.array[i].tax_rate_corporate;
+				TAX_RATE_HH_LABOUR = eurostat_tax_rates_message->government_tax_rates.array[i].tax_rate_hh_labour;
+				TAX_RATE_HH_CAPITAL = eurostat_tax_rates_message->government_tax_rates.array[i].tax_rate_hh_capital;
+				TAX_RATE_VAT = eurostat_tax_rates_message->government_tax_rates.array[i].tax_rate_vat;
+				break;
+			}
+		}
+		FINISH_EUROSTAT_TAX_RATES_MESSAGE_LOOP
+*/		
+		//Message send by Government:
+		START_GOVERNMENT_TAX_RATES_MESSAGE_LOOP			
+			if(government_tax_rates_message->gov_id == GOV_ID)
+			{
+				TAX_RATE_CORPORATE = government_tax_rates_message->tax_rate_corporate;
+				TAX_RATE_HH_LABOUR = government_tax_rates_message->tax_rate_hh_labour;
+				TAX_RATE_HH_CAPITAL = government_tax_rates_message->tax_rate_hh_capital;
+				TAX_RATE_VAT = government_tax_rates_message->tax_rate_vat;
+			}
+		FINISH_GOVERNMENT_TAX_RATES_MESSAGE_LOOP
 
+		return 0;
 }
 
 /** \fn Firm_calculate_specific_skills_and_wage_offer()
