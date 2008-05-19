c4443adcb2e101199fe9f44eda736e47d7e7375e
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-05-19 14:31:28 +0000

    Statistical_Office_UNIBI: All descriptions, added message filters in xml, removed them from C.

--- models/EURACE_Model/trunk/Statistical_Office_UNIBI/Firm_Statistics_Functions.c
+++ models/EURACE_Model/trunk/Statistical_Office_UNIBI/Firm_Statistics_Functions.c
@@ -1,5 +1,5 @@
 /** \fn Firm_send_data_to_Eurostat()
- * \brief Firms send data to Market Research: controlling results and creating macro data
+ * \brief Firm sends message to the eurostat agent including several micro data.
  */
 int Firm_send_data_to_Eurostat()
 {
@@ -16,6 +16,9 @@ int Firm_send_data_to_Eurostat()
 	return 0;
 }
 
+/** \fn Firm_send_payments_to_bank()
+ * \brief Daily payment account update.
+ */
 int Firm_send_payments_to_bank()
 {
 	add_bank_account_update_message(PAYMENT_ACCOUNT);
@@ -23,24 +26,22 @@ int Firm_send_payments_to_bank()
 }
 
 /** \fn Firm_read_tax_rates()
- * \brief Firms reads the tax_rates_messages from Governments
+ * \brief Firms reads the tax_rates_message from its Government (msg has been filtered).
  */
 int Firm_read_tax_rates()
 {
 	//Message send by Government:
 	START_GOVERNMENT_TAX_RATES_MESSAGE_LOOP			
-		if(government_tax_rates_message->gov_id == GOV_ID)
-		{
-			TAX_RATE_CORPORATE = government_tax_rates_message->tax_rate_corporate;
-			TAX_RATE_VAT = government_tax_rates_message->tax_rate_vat;
-		}
+		TAX_RATE_CORPORATE = government_tax_rates_message->tax_rate_corporate;
+		TAX_RATE_VAT = government_tax_rates_message->tax_rate_vat;
 	FINISH_GOVERNMENT_TAX_RATES_MESSAGE_LOOP
 
 	return 0;
 }
 
 /** \fn Firm_receive_data()
- * \brief Firms receive the data messages from the Eurostat*/
+ * \brief The firm reads the messages from the eurostat agent and stores the needed data.
+ */
 int Firm_receive_data()
 {
 					
@@ -50,8 +51,8 @@ int Firm_receive_data()
 	START_EUROSTAT_SEND_SPECIFIC_SKILLS_MESSAGE_LOOP
 		
 		/*Specific skills of the domestic region*/
-		if(eurostat_send_specific_skills_message->region_id == REGION_ID)
-		{
+//		if(eurostat_send_specific_skills_message->region_id == REGION_ID)
+//		{
 			/*If there is no employee with general skill level 1 resp. 2-5*/
 			if(NO_EMPLOYEES_SKILL_1 == 0)
 			{
@@ -82,7 +83,7 @@ int Firm_receive_data()
 				AVERAGE_S_SKILL_OF_5 = 						eurostat_send_specific_skills_message->
 				specific_skill_5;
 			}
-		}
+//		}
 		FINISH_EUROSTAT_SEND_SPECIFIC_SKILLS_MESSAGE_LOOP
 
 		return 0;
--- models/EURACE_Model/trunk/Statistical_Office_UNIBI/Household_Statistics_Functions.c
+++ models/EURACE_Model/trunk/Statistical_Office_UNIBI/Household_Statistics_Functions.c
@@ -8,17 +8,14 @@
 /*************************************Household Role: Statistics *********************************/
 
 /** \fn Household_read_tax_rates()
- * \brief Household reads the tax_rates_messages from Governments
+ * \brief Function to read the tax_rates_message from Government (msg has been filtered).
  */
 int Household_read_tax_rates()
 {
 	//Message send by Government:
 	START_GOVERNMENT_TAX_RATES_MESSAGE_LOOP			
-		if(government_tax_rates_message->gov_id == GOV_ID)
-		{
-			TAX_RATE_HH_LABOUR = government_tax_rates_message->tax_rate_hh_labour;
-			TAX_RATE_HH_CAPITAL = government_tax_rates_message->tax_rate_hh_capital;
-		}
+		TAX_RATE_HH_LABOUR = government_tax_rates_message->tax_rate_hh_labour;
+		TAX_RATE_HH_CAPITAL = government_tax_rates_message->tax_rate_hh_capital;
 	FINISH_GOVERNMENT_TAX_RATES_MESSAGE_LOOP
 
 	return 0;
