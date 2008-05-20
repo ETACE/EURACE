3b760f2803f33b7d0c280271ce4f4070d85aeffa
Simon Coakley <869932+sysfal@users.noreply.github.com>
2008-05-20 09:26:54 +0000

    commit of simple test model 01

---
+++ FLAME/branches/simon/test_model_01/agent_b_functions.c
@@ -0,0 +1,22 @@
+#include "header.h"
+#include "agent_b_agent_header.h"
+
+/** \fn agent_b_1()
+ * \brief A description of the function
+ */
+int agent_b_1()
+{
+   /* code here */
+
+   return 0;
+}
+
+/** \fn agent_b_2()
+ * \brief A description of the function
+ */
+int agent_b_2()
+{
+   /* code here */
+
+   return 0;
+}
