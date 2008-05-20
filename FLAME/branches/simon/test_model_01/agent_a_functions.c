9294084aa98c9901f9b8401b2e446e6950c2a1fa
sysfal <sysfal@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-05-20 09:26:54 +0000

    commit of simple test model 01

---
+++ FLAME/branches/simon/test_model_01/agent_a_functions.c
@@ -0,0 +1,22 @@
+#include "header.h"
+#include "agent_a_agent_header.h"
+
+/** \fn agent_a_1()
+ * \brief A description of the function
+ */
+int agent_a_1()
+{
+   /* code here */
+
+   return 0;
+}
+
+/** \fn agent_a_2()
+ * \brief A description of the function
+ */
+int agent_a_2()
+{
+   /* code here */
+
+   return 0;
+}
