368f73ab6bf81bcf8cee4ed2be7de07e86cf8445
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-06-19 13:30:14 +0000

    Original branch from Model_UNIBI_after_Genoa@1042 to Model_UNIBI_IGFirm

---
+++ models/ALM/alm_branches/Model_UNIBI_IGFirm/Statistical_Office_UNIBI/IGFirm_Statistics_Functions.c
@@ -0,0 +1,40 @@
+#include "../header.h"
+#include "../IGFirm_agent_header.h"
+#include "../my_library_header.h"
+
+
+
+
+/** \fn IGFirm_send_data_to_Eurostat()
+ * \brief IGFirms send data to Market Research: controlling results and creating macro data
+ */
+int IGFirm_send_data_to_Eurostat()
+{
+	return 0;
+}
+
+
+/** \fn IGFirm_read_tax_rates()
+ * \brief IGFirms reads the tax_rates_messages from Governments
+ */
+int IGFirm_read_tax_rates()
+{
+	//Message send by Government:
+	START_GOVERNMENT_TAX_RATES_MESSAGE_LOOP			
+		if(government_tax_rates_message->gov_id == GOV_ID)
+		{
+			TAX_RATE_CORPORATE = government_tax_rates_message->tax_rate_corporate;
+			TAX_RATE_VAT = government_tax_rates_message->tax_rate_vat;
+		}
+	FINISH_GOVERNMENT_TAX_RATES_MESSAGE_LOOP
+
+	return 0;
+}
+
+
+/** \fn IGFirm_receive_data()
+ * \brief IGFirms receive the data messages from the Eurostat*/
+int IGFirm_receive_data()
+{
+	return 0;
+}
