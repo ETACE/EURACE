281dde99266d5d54c40edfeb3a2b83bcb911c879
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-07-01 06:45:42 +0000

    afm_branches/Model_Sheffield: deleted faulty branches

--- models/AFM/afm_branches/Model_Sheffield/CGM_UNIBI/unittests/compile_unittest.bat
+++
@@ -1,1 +0,0 @@
-gcc -g unittest.c ../../memory.c ../../messageboards.c ../../my_library_functions.c ../Firm_Producer_functions.c Firm_Producer_unittests.c -lm -lcunit 
--- models/AFM/afm_branches/Model_Sheffield/IGM_UNIBI/unittests/compile_unittest.bat
+++
@@ -1,1 +0,0 @@
-gcc -g unittest.c ../../memory.c ../../messageboards.c ../../my_library_functions.c ../InvGoodFirm_functions.c InvGoodFirm_unittests.c -lm -lcunit 
