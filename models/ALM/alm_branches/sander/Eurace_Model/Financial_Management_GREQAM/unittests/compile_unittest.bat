ed2f310215fb66591ff1932242447cfd37beca58
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-07-02 17:15:56 +0000

    maintenance: removed old branches

--- models/ALM/alm_branches/sander/Eurace_Model/Financial_Management_GREQAM/unittests/compile_unittest.bat
+++
@@ -1,1 +0,0 @@
-gcc -g unittest.c ../../memory.c ../../messageboards.c ../Firm_Financial_Management_Functions.c Firm_Financial_Management_unittests.c -lm -lcunit 
--- models/ALM/alm_branches/sander/Eurace_Model/Labour_UNIBI/unittests/compile_unittest.bat
+++
@@ -1,1 +0,0 @@
-gcc -g unittest.c ../../memory.c ../../messageboards.c ../../my_library_functions.c ../Firm_Employer_functions.c Firm_Employer_unittests.c -lm -lcunit 
