d72f936888686cecb2a879bbeb470437b44fd723
Simon Coakley <869932+sysfal@users.noreply.github.com>
2008-07-10 15:41:47 +0000

    updated to include libmboard (in the directory above)

--- models/AFM/afm_branches/initial/ewa_learning/unittests/compile_unittest_aux.bat
+++ models/AFM/afm_branches/initial/ewa_learning/unittests/compile_unittest_aux.bat
@@ -1,1 +1,1 @@
-gcc -g -Ilibmboard/include -Llibmboard/lib unittest_aux.c ../memory.c ../messageboards.c ../mylibraryfunctions.c ../some_new_functions.c  some_new_functions_unittest.c -lm -lcunit -lmboard_s 
+gcc -g -I../libmboard/include -L../libmboard/lib unittest_aux.c ../memory.c ../messageboards.c ../mylibraryfunctions.c ../some_new_functions.c  some_new_functions_unittest.c -lm -lcunit -lmboard_s 
