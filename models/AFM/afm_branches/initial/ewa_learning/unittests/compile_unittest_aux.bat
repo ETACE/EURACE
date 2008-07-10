3166d4123af8642e11513f81690139cce55e3899
sysfal <sysfal@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-07-10 15:40:49 +0000

    updated to include libmboard

--- models/AFM/afm_branches/initial/ewa_learning/unittests/compile_unittest_aux.bat
+++ models/AFM/afm_branches/initial/ewa_learning/unittests/compile_unittest_aux.bat
@@ -1,1 +1,1 @@
-gcc -g unittest_aux.c ../memory.c ../messageboards.c ../mylibraryfunctions.c ../some_new_functions.c  some_new_functions_unittest.c -lm -lcunit 
+gcc -g -Ilibmboard/include -Llibmboard/lib unittest_aux.c ../memory.c ../messageboards.c ../mylibraryfunctions.c ../some_new_functions.c  some_new_functions_unittest.c -lm -lcunit -lmboard_s 
