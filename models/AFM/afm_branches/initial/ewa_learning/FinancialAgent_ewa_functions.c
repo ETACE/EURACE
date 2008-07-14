77a36043925cafeadfb5e4e13b2b41ddaf0836e6
svdhoog <11057058+svdhoog@users.noreply.github.com>
2008-07-14 08:18:15 +0000

    edits EWA

--- models/AFM/afm_branches/initial/ewa_learning/FinancialAgent_ewa_functions.c
+++ models/AFM/afm_branches/initial/ewa_learning/FinancialAgent_ewa_functions.c
@@ -53,6 +53,48 @@ int FinancialAgent_send_all_performances()
   return 0;
 }
 
+/* \fn FinancialAgent_apply_GA_refactored()
+ * \brief FinancialAgent applies the GA to the rule details parameters.
+ */
+int FinancialAgent_apply_GA_refactored()
+{
+	// Prototypes:
+	//int GA_selection(int * parent_index_1, int * parent_index_2, int * rule_id_1, int * rule_id_2);
+	//void GA_reproduction(int id_1, int id_2, double * offspring_1, double * offspring_2);
+	//void GA_mutation(int size, double * offspring_1, double * offspring_2);
+	//void GA_election(int size, double * offspring_1, double * offspring_2);
+	//void GA_reinsertion(int size, double * offspring_1, double * offspring_2, int * id1, int * id2);
+
+	int j, N_pairs;
+	double * offspring_1; //copy from PUBLIC_CLASSIFIERSYSTEM.ruletable[id1]
+	double * offspring_2; //copy from PUBLIC_CLASSIFIERSYSTEM.ruletable[id2]
+	int * parent_index_1; //contains random draws from interval [0, N_pairs], used for random matching
+	int * parent_index_2; //contains random draws from interval [0, N_pairs], used for random matching
+	int * rule_id_1;      //contains the rule_ids from PUBLIC_CLASSIFIERSYSTEM.ruletable[id1]
+	int * rule_id_2;
+	
+	//uncomment this:
+	//N_pairs = GA_selection(parent_index_1, parent_index_2, rule_id_1, rule_id_2);
+	for (j=0; j<N_pairs; j++)
+	{    
+		GA_reproduction(rule_id_1[j], rule_id_2[j], offspring_1, offspring_2);
+		GA_mutation(NR_PARAMS, offspring_1, offspring_2);
+		GA_election(NR_PARAMS, offspring_1, offspring_2);
+		GA_reinsertion(NR_PARAMS, offspring_1, offspring_2, rule_id_1[j], rule_id_2[j]);
+	}
+
+	// free all allocated memory
+	free(offspring_1);
+	free(offspring_2);
+	free(parent_index_1);
+	free(parent_index_2);	
+	free(rule_id_1);
+	free(rule_id_2);
+
+	  return 0;
+}
+
+
 /* \fn FinancialAgent_apply_GA()
  * \brief FinancialAgent applies the GA to the rule details parameters.
  */
@@ -89,14 +131,6 @@ int FinancialAgent_apply_GA()
 	
 	int cross_point, cross_length;
 		
-	//Date-event triggered: every 240 days run the GA (condition in xml)
-	//GA CODE HERE:
-	//void GA_selection();
-	//void GA_reproduction();
-	//void GA_mutation();
-	//void GA_election();
-	//void GA_reinsertion();	
-	
     /*********************** Start of Selection function **************************************/
 	//void GA_selection()
 	// 1. Selection/Reproduction
@@ -137,6 +171,8 @@ int FinancialAgent_apply_GA()
      
     // Drawing N_rep random copies (without replacement) from the entire population
     // using the fitness-based probabilities ('draws' contains indices of drawn rules)
+    //WARNING: draws contains indices (int), but draw_without_replacement() returns a double array.
+    //Is this correctly type-cast inside the function draw_without_replacement() when it fills in draws?
      draws = malloc(sizeof(double)*N_rep);
      draw_without_replacement(nr_rules, cpdf, N_rep, draws);
 	
@@ -145,89 +181,103 @@ int FinancialAgent_apply_GA()
      parent_index_1 = malloc(sizeof(int)*N_pairs);
      parent_index_2 = malloc(sizeof(int)*N_pairs);
      
-    // Drawing is with replacement (so same string can be both parents) using uniform probabilities:
+    // Drawing is with replacement using uniform probabilities from the discrete interval: [0, N_pairs+1]
+     //(both parents can be the same bitstring) 
+     //We use (N_pairs+0.999) to allow the last string to be drawn (casting as int takes the floor of the double).
+     //void draw_with_replacement(int size, double * vector, int nr_draws, double * draws)
+     //draw_with_replacement(N_pairs, double * vector, N_pairs, parent_index_1)
+     
      for (j=0; j<N_pairs; j++)
      {
     	 parent_index_1[j] = (int)((N_pairs+0.999)*random_unif());
     	 parent_index_2[j] = (int)((N_pairs+0.999)*random_unif());
+
+    	 // Now we have selected the indices of N_pairs parent pairs that will undergo crossover and mutation.
+         // To retrieve the 2 rule_ids associated to these two indices parent_index_1, parent_index_2
+         // we need to get them from 'draws'.
+
+    	 // Rule indices id1, id2 are stored in the vector 'draws':
+    	 index=parent_index_1[j];
+     	 id1=draws[index];
+ 	    	
+     	 index=parent_index_2[j];
+     	 id2=draws[index];
      }
+	 //Now we have the correct 2 rule_ids that are randomly matched, and can be used in the following functions.     
      /*********************** End of Selection function **************************************/
      
      /*********************** Start of Reproduction function **************************************************/
- 	 //void GA_reproduction()
+ 	 //void GA_reproduction(j): applies to each pair, nr_pair j=0,...,N_pairs
      //2. Genetic operators: cross-over
      for (j=0; j<N_pairs; j++)
-     {
-    	//rule indices id1, id2 are stored in the vector 'draws':
-		index=parent_index_1[j];
-    	id1=draws[index];
-    	
-    	index=parent_index_2[j];
-		id2=draws[index];
-		
+     {			
 		//make a copy of the parents
 		offspring_1 = malloc(sizeof(double)*NR_PARAMS);
 		offspring_2 = malloc(sizeof(double)*NR_PARAMS);
-
+	
 		for (k=0; k<NR_PARAMS; k++)
 		{
 			offspring_1[k]=PUBLIC_CLASSIFIERSYSTEM.ruletable[id1].parameters[k];
 			offspring_2[k]=PUBLIC_CLASSIFIERSYSTEM.ruletable[id2].parameters[k];
 		}
 		
-		//now cross-over the strings
+		//Cross-over the strings offspring_1 and offspring_2
 		if (random_unif() > GA_PARAMETERS.prob_cross)
 		{
 			// 2a. Cross-over: 1-point cross-over
 			if (GA_PARAMETERS.single_point_cross_over)
 			{
-    			// draw random cross-over point between $[1,L-1]$
-    			cross_point = (int)random_unif_interval(1, NR_PARAMS-1);	
-    			single_point_cross_over(NR_PARAMS, offspring_1, offspring_2, cross_point);
+				// draw random cross-over point between $[1,L-1]$
+				cross_point = (int)random_unif_interval(1, NR_PARAMS-1);	
+				single_point_cross_over(NR_PARAMS, offspring_1, offspring_2, cross_point);
 			}
 			else
 			{
 			    // 2b. Cross-over: 2-point cross-over
 				// draw random cross-over point between $[1,L-1]$
-    			cross_point = (int)random_unif_interval(1, NR_PARAMS-1);
+				cross_point = (int)random_unif_interval(1, NR_PARAMS-1);
 				
 				// draw random cross-over length between $[1,L-1]$
-    			cross_length = (int)random_unif_interval(1, NR_PARAMS-1);
+				cross_length = (int)random_unif_interval(1, NR_PARAMS-1);
 				
-    			two_point_cross_over(NR_PARAMS, offspring_1, offspring_2, cross_point, cross_length);
-    		}
+				two_point_cross_over(NR_PARAMS, offspring_1, offspring_2, cross_point, cross_length);
+			}
 		}
 		else
 		{
 	        //No cross-over occurs: 2 offspring remain identical copies of the parents
 		}
+     
 		/*********************** End of Reproduction function **************************************************/
 		
-	    /*********************** Start of Mutation function ****************************************************/
-		//void GA_mutation()
+		/*********************** Start of Mutation function ****************************************************/
+		//void GA_mutation(int size, double * offspring_1, double * offspring_2): applies to each pair, nr_pair j=0,...,N_pairs
 		// 3. Mutation: each bit has a probability of mutating by a stepsize
-		//void mutation(int size, double * offspring_1, double * offspring_2);
+	 	//For each of the strings offspring_1 and offspring_2 that have just undergone cross-over, now perform mutation
+	
+	    //void mutation(int size, double * offspring_1, double * offspring_2);
 		mutation(NR_PARAMS, offspring_1, GA_PARAMETERS.stepsize, GA_PARAMETERS.prob_mut);
 		mutation(NR_PARAMS, offspring_2, GA_PARAMETERS.stepsize, GA_PARAMETERS.prob_mut);
-	    /*********************** End of Mutation function ****************************************************/
+	
+		/*********************** End of Mutation function ****************************************************/
 		
 	    /*********************** Start of Election function ****************************************************/		
-		//void GA_election() 
+		//void GA_election() : applies to each pair, nr_pair j=0,...,N_pairs
 		// 4. Election: before 2 offsprings are added to the gene pool, they are tested against their parents.
 		// Out ot the 4 candidates (2 parents, 2 offsprings) the best 4 are retained.
 		if (GA_PARAMETERS.election)
 		{
 			//1. Make 2 local copies of the parents
-    		/*
-    		 parent_1 = malloc(sizeof(double)*NR_PARAMS);
+			/*
+			 parent_1 = malloc(sizeof(double)*NR_PARAMS);
 			 parent_2 = malloc(sizeof(double)*NR_PARAMS);
-
+	
 			for (k=0; k<NR_PARAMS; k++)
-    		{
-     		    parent_1[k]=PUBLIC_CLASSIFIERSYSTEM.ruletable[id1].parameters[k];
-     		    parent_2[k]=PUBLIC_CLASSIFIERSYSTEM.ruletable[id2].parameters[k];
-    		}
-    		*/
+			{
+	 		    parent_1[k]=PUBLIC_CLASSIFIERSYSTEM.ruletable[id1].parameters[k];
+	 		    parent_2[k]=PUBLIC_CLASSIFIERSYSTEM.ruletable[id2].parameters[k];
+			}
+			*/
 			
 			//2. Compare the 2 offspring to their 2 parents: test for higher fitness between 2 offspring and 2 parents
 			// void election(int size, double * offspring_1, double * offspring_2, double * parent_1, double * parent_2)
@@ -239,16 +289,16 @@ int FinancialAgent_apply_GA()
 	    /*********************** End of Election function ****************************************************/
 		
 	    /*********************** Start of Replacement function ****************************************************/
-		//void GA_reinsertion()
+		//void GA_reinsertion() : applies to each pair
 		//5. Finally, add the new strings to the population to replace the old ones
 		//This means: copy the parameters into the classifier system
 		for (k=0; k<NR_PARAMS; k++)
 		{
 			PUBLIC_CLASSIFIERSYSTEM.ruletable[id1].parameters[k] = offspring_1[k];
-    		PUBLIC_CLASSIFIERSYSTEM.ruletable[id2].parameters[k] = offspring_2[k];
+			PUBLIC_CLASSIFIERSYSTEM.ruletable[id2].parameters[k] = offspring_2[k];
 		}
 		/*********************** End of Replacement function ****************************************************/
-     }		
+	 }		
 
 	free(p);
 	free(cpdf);
--- models/AFM/afm_branches/initial/ewa_learning/some_new_functions.c
+++ models/AFM/afm_branches/initial/ewa_learning/some_new_functions.c
@@ -1,6 +1,8 @@
 #include "header.h"
 #include "mylibraryheader.h"
 #include "my_library_header.h"
+#include "FinancialAgent_agent_header.h"
+
 //*********** BEGIN AUXILIARY FUNCTIONS ****************************
 
 //double sum(double * p, int size)
@@ -24,14 +26,14 @@ double sum(double * p, int size)
 	int i;
 	double sum_val;
 
-	printf("\n In sum_unittest: p=[%1.1f, %1.1f, %1.1f, %1.1f]\n", p[0], p[1], p[2], p[3]);
+//	printf("\n In function sum: p=[%1.1f, %1.1f, %1.1f, %1.1f]\n", p[0], p[1], p[2], p[3]);
 
 	sum_val=0.0;
 	for (i=0;i<size;i++)
 	{
 		sum_val += p[i];
 	}	
-	printf("\n In function sum: sum_val=%f", sum_val);
+//	printf("\n In function sum: sum_val=%f", sum_val);
 	
     return(sum_val);
 }
@@ -469,5 +471,214 @@ void election(int size, double * offspring_1, double * offspring_2, double * par
 
 }
 */
-
 // *********** END AUXILIARY FUNCTIONS ****************************
+
+// *********** BEGIN GA AUXILIARY FUNCTIONS ****************************
+/*
+int GA_selection(int * parent_index_1, int * parent_index_2, int * rule_id1, int * rule_id2)
+{
+	int j, index;
+	int nr_rules, N_rep, N_pairs;
+	double avg_performance, sum;
+	
+	double * p;
+	double * cpdf;
+	double * draws;
+		
+	// *********************** Start of Selection function **************************************
+	// 1. Selection/Reproduction
+			
+	// N_rep is some fixed percentage of the population size pop_size, and should be even.
+	N_rep = (int) 2*floor((GA_PARAMETERS.reproduction_proportion * GA_PARAMETERS.pop_size)/2);
+
+    //Computing fitness-based probabilities using multi-logit probabilities
+	nr_rules = PUBLIC_CLASSIFIERSYSTEM.nr_rules;
+	sum=0.0;
+    for (j=0;j<nr_rules;j++)
+    {
+    	avg_performance  = PUBLIC_CLASSIFIERSYSTEM.ruletable[j].avg_performance;
+        sum += exp(EWA_PARAMETERS.EWA_beta * avg_performance);
+    }
+    
+    p = malloc(sizeof(double)*nr_rules);
+    for (j=0;j<nr_rules;j++)
+    {
+    	avg_performance  = PUBLIC_CLASSIFIERSYSTEM.ruletable[j].avg_performance;
+        p[j] = exp(EWA_PARAMETERS.EWA_beta * avg_performance)/sum;
+    }
+
+    // Construct cumulative probability density function: cpdf
+     cpdf = malloc(sizeof(double)*nr_rules);
+     cumpdf(p, nr_rules, cpdf);
+     
+    //print prob. vector:
+     printf("\n prob: [");
+     for (j=0;j<nr_rules;j++){printf("%2.2f ", p[j]);}
+     printf("]\n");
+          
+    //print cpdf:
+     printf("\n cpdf: [");
+     for (j=0;j<nr_rules;j++){printf("%2.2f ", cpdf[j]);}
+     printf("]\n"); 
+     
+     
+     // Drawing N_rep random copies (without replacement) from the entire population
+     // using the fitness-based probabilities ('draws' contains the indices of the selected rules)
+     //WARNING: draws contains indices (int), but draw_without_replacement() returns a double array.
+     //Is this correctly type-cast inside the function draw_without_replacement() when it fills in draws?
+     draws = malloc(sizeof(int)*N_rep);
+     draw_without_replacement(nr_rules, cpdf, N_rep, draws);
+	
+	 // Create N_pairs parent pairs by random matching from the N_rep draws (N_pairs = (int) 0.5*N_rep)
+     N_pairs = (int) 0.5*N_rep;
+     parent_index_1 = malloc(sizeof(int)*N_pairs);
+     parent_index_2 = malloc(sizeof(int)*N_pairs);
+     
+     // For the random matching, drawing is WITH replacement using uniform probabilities from the discrete interval: [0, N_pairs+1]
+     // Drawing with replacement allows for both parents to be a copy of the same bitstring.
+     // We use (N_pairs+0.999) below to allow also the last string to be drawn (casting as int takes the floor of the double).
+     for (j=0; j<N_pairs; j++)
+     {
+    	 parent_index_1[j] = (int)((N_pairs+0.999)*random_unif());
+    	 parent_index_2[j] = (int)((N_pairs+0.999)*random_unif());
+     
+    	 //equivalent code:
+    	 // parent_index_1[j] = (int)(random_unif_interval(0.0, N_pairs+0.999));
+    	 // parent_index_2[j] = (int)(random_unif_interval(0.0, N_pairs+0.999));
+
+     // Now we have selected the indices of N_pairs parent pairs that will undergo crossover and mutation.
+     // To retrieve the 2 rule_ids associated to these two indices parent_index_1, parent_index_2
+     // we need to get them from 'draws'.
+
+	 // Rule indices id1, id2 are stored in the vector 'draws':
+		index=parent_index_1[j];
+		rule_id1[j]=draws[index];
+		
+		index=parent_index_2[j];
+		rule_id2[j]=draws[index];
+     }
+
+	 //Now we have the correct 2 rule_ids that are randomly matched, and can be used in the following functions.
+     // *********************** End of Selection function **************************************
+
+    free(p);
+ 	free(cpdf);
+ 	free(draws);
+}
+*/
+
+void GA_reproduction(int id1, int id2, double * offspring_1, double * offspring_2)
+{
+	int k;
+	int cross_point, cross_length;
+
+    /*********************** Start of Reproduction function **************************************************/
+	 //void GA_reproduction() : applies to each parent pair, nr_pair=0,...,N_pairs
+     //2. Genetic operators: cross-over
+		
+		//make a copy of the parents
+		offspring_1 = malloc(sizeof(double)*NR_PARAMS);
+		offspring_2 = malloc(sizeof(double)*NR_PARAMS);
+	
+		for (k=0; k<NR_PARAMS; k++)
+		{
+			offspring_1[k]=PUBLIC_CLASSIFIERSYSTEM.ruletable[id1].parameters[k];
+			offspring_2[k]=PUBLIC_CLASSIFIERSYSTEM.ruletable[id2].parameters[k];
+		}
+
+		//now cross-over the strings
+		if (random_unif() > GA_PARAMETERS.prob_cross)
+		{
+			// 2a. Cross-over: 1-point cross-over
+			if (GA_PARAMETERS.single_point_cross_over)
+			{
+	   			// draw random cross-over point between $[1,L-1]$
+	   			cross_point = (int)random_unif_interval(1, NR_PARAMS-1);	
+	   			single_point_cross_over(NR_PARAMS, offspring_1, offspring_2, cross_point);
+			}
+			else
+			{
+			    // 2b. Cross-over: 2-point cross-over
+				// draw random cross-over point between $[1,L-1]$
+				cross_point = (int)random_unif_interval(1, NR_PARAMS-1);
+				
+				// draw random cross-over length between $[1,L-1]$
+				cross_length = (int)random_unif_interval(1, NR_PARAMS-1);
+				
+				two_point_cross_over(NR_PARAMS, offspring_1, offspring_2, cross_point, cross_length);
+			}
+		}
+		else
+		{
+	        //No cross-over occurs: 2 offspring remain identical copies of the parents
+		}
+	/*********************** End of Reproduction function **************************************************/
+}
+
+void GA_mutation(int size, double * offspring_1, double * offspring_2)
+{
+	
+	/*********************** Start of Mutation function ****************************************************/
+	//void GA_mutation(int size, double * offspring_1, double * offspring_2): applies to each pair, nr_pair j=0,...,N_pairs
+	// 3. Mutation: each bit has a probability of mutating by a stepsize
+ 	//For each of the strings offspring_1 and offspring_2 that have just undergone cross-over, now perform mutation
+
+    //void mutation(int size, double * offspring_1, double * offspring_2);
+	mutation(NR_PARAMS, offspring_1, GA_PARAMETERS.stepsize, GA_PARAMETERS.prob_mut);
+	mutation(NR_PARAMS, offspring_2, GA_PARAMETERS.stepsize, GA_PARAMETERS.prob_mut);
+
+	/*********************** End of Mutation function ****************************************************/
+}
+
+void GA_election(int size, double * offspring_1, double * offspring_2)
+{
+	int k;
+	double * parent_1;
+	double * parent_2;
+
+    /*********************** Start of Election function ****************************************************/		
+	//void GA_election() : applies to each pair, nr_pair j=0,...,N_pairs
+	// 4. Election: before 2 offsprings are added to the gene pool, they are tested against their parents.
+	// Out ot the 4 candidates (2 parents, 2 offsprings) the best 4 are retained.
+	if (GA_PARAMETERS.election)
+	{
+		//1. Make 2 local copies of the parents
+		/*
+		 parent_1 = malloc(sizeof(double)*NR_PARAMS);
+		 parent_2 = malloc(sizeof(double)*NR_PARAMS);
+
+		for (k=0; k<NR_PARAMS; k++)
+		{
+ 		    parent_1[k]=PUBLIC_CLASSIFIERSYSTEM.ruletable[id1].parameters[k];
+ 		    parent_2[k]=PUBLIC_CLASSIFIERSYSTEM.ruletable[id2].parameters[k];
+		}
+		*/
+		
+		//2. Compare the 2 offspring to their 2 parents: test for higher fitness between 2 offspring and 2 parents
+		// void election(int size, double * offspring_1, double * offspring_2, double * parent_1, double * parent_2)
+		// election(NR_PARAMS, offspring_1, offspring_2, parent_1, parent_2);
+		
+		//3. Add 2 out of 4 best bitstrings to new generation
+		
+	}
+    /*********************** End of Election function ****************************************************/
+ 	free(parent_1);
+ 	free(parent_2);
+}
+
+void GA_reinsertion(int size, double * offspring_1, double * offspring_2, int id1, int id2)
+{
+	int k;
+	
+    /*********************** Start of Reinsertion function ****************************************************/
+	//void GA_reinsertion() : applies to each pair
+	//5. Finally, add the new strings to the population to replace the old ones
+	//This means: copy the parameters into the classifier system
+	for (k=0; k<NR_PARAMS; k++)
+	{
+		PUBLIC_CLASSIFIERSYSTEM.ruletable[id1].parameters[k] = offspring_1[k];
+		PUBLIC_CLASSIFIERSYSTEM.ruletable[id2].parameters[k] = offspring_2[k];
+	}
+	/*********************** End of Reinsertion function ****************************************************/
+}
+// *********** END GA AUXILIARY FUNCTIONS ****************************
