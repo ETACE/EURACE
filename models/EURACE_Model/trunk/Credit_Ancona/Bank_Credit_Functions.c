e8013070ebf8e68e34f000c6831fac662a423fef
Simon Coakley <869932+sysfal@users.noreply.github.com>
2008-07-15 15:33:21 +0000

    fix variable names

--- models/EURACE_Model/trunk/Credit_Ancona/Bank_Credit_Functions.c
+++ models/EURACE_Model/trunk/Credit_Ancona/Bank_Credit_Functions.c
@@ -1,6 +1,6 @@
-#include "header.h"
-#include "Bank_agent_header.h"
-#include "my_library_header.h"
+#include "../header.h"
+#include "../Bank_agent_header.h"
+#include "../my_library_header.h"
 
 int Bank_decide_credit_conditions()
 {
@@ -15,15 +15,15 @@ int Bank_decide_credit_conditions()
 			c = loan_request_message->credit_demand;
 			bankruptcy_prob = 1-exp(-(d+c)/e);
 			r = bankruptcy_prob*c/e;
-			if ( VAR+r <= ALFA*EQUITY ) 
+			if ( VALUE_AT_RISK+r <= ALFA*EQUITY ) 
 			{
 				credit_allowed = c;
 			}
 			else 
 			{
-				credit_allowed = (ALFA*EQUITY - VAR)/bankruptcy_prob;
+				credit_allowed = (ALFA*EQUITY - VALUE_AT_RISK)/bankruptcy_prob;
 			}
-			i = MIN_INTEREST + OMEGA[0]*r;
+			i = MIN_INTEREST + BANK_GAMMA[0]*r;
 			add_loan_conditions_message(loan_request_message->firm_id, ID, i, credit_allowed,  r*(c/credit_allowed));
 		FINISH_LOAN_REQUEST_MESSAGE_LOOP
 	}
@@ -64,10 +64,10 @@ int Bank_receive_installment()
 {
 	START_INSTALLMENT_MESSAGE_LOOP
 
-		CASH +=installment_message->installment_amount_paid;
-        PROFITS[0] += installment_message->interest;
-        EQUITY += installment_message->interest;
-        VAR -= installment_message->var_per_instalment;
+		CASH +=installment_message->installment_amount;
+        PROFITS[0] += installment_message->interest_amount;
+        EQUITY += installment_message->interest_amount;
+        VALUE_AT_RISK -= installment_message->var_per_installment;
 
 	FINISH_INSTALLMENT_MESSAGE_LOOP
 
@@ -76,7 +76,7 @@ int Bank_receive_installment()
        CASH +=bankruptcy_message->credit_refunded;
        EQUITY -= bankruptcy_message->bad_debt;
        PROFITS[0] -= bankruptcy_message->bad_debt;
-       VAR -= bankruptcy_message->residual_var;
+       VALUE_AT_RISK -= bankruptcy_message->residual_var;
 
     FINISH_BANKRUPTCY_MESSAGE_LOOP
 	
@@ -104,35 +104,35 @@ int Bank_accounting()
 
      double q, c, gro, tax_bank, total_dividends, dividend_per_share;   
 
-     if (PROFIT[1]!=0)
+     if (PROFITS[1]!=0)
      {
-        gro=(PROFIT[0]-PROFIT[1])/PROFIT[1]);
+        gro=( (PROFITS[0]-PROFITS[1])/PROFITS[1] );
      }
 
      else  
      gro=0;
  
-     PROFIT[1]=PROFIT[0]; //update
-     q=OMEGA[0]; 
-     c=OMEGA[1];
-     OMEGA[0]=q+lambda*(q-c)*gro+(rand()/RAND_MAX)*0.01;
+     PROFITS[1]=PROFITS[0]; //update
+     q=BANK_GAMMA[0]; 
+     c=BANK_GAMMA[1];
+     BANK_GAMMA[0]=q+BANK_LAMBDA*(q-c)*gro+(rand()/RAND_MAX)*0.01;
  
-     if (OMEGA[0]<0.02)
+     if (BANK_GAMMA[0]<0.02)
      {
-         OMEGA[0]=0.02;
+         BANK_GAMMA[0]=0.02;
      }
 
      // tax and dividends payment
-     if (PROFIT[0]>0)
+     if (PROFITS[0]>0)
      {
-         tax_bank = TAX_RATE_CORPORATE*PROFIT[0];
-         PROFIT[0] -= tax_bank;
+         tax_bank = TAX_RATE_CORPORATE*PROFITS[0];
+         PROFITS[0] -= tax_bank;
          EQUITY -= tax_bank;
          CASH -= tax_bank;
          add_tax_payment_message(ID, GOV_ID, tax_bank);  
-         total_dividends = DIVIDEND_RATE*PROFIT[0];
+         total_dividends = DIVIDEND_RATE*PROFITS[0];
          dividend_per_share = total_dividends/NUMBER_OF_SHARES; 
-         PROFIT[0] -= total_dividends;
+         PROFITS[0] -= total_dividends;
          EQUITY -= total_dividends;
          CASH -= total_dividends;
          add_dividend_per_share_message(ID, dividend_per_share);                  
@@ -144,7 +144,7 @@ int Bank_accounting()
          CASH = 0;
      }
      
-     PROFIT[0]=0;  //update
+     PROFITS[0]=0;  //update
 
       return 0;
 }
