90fab4e61550182f3f7347f9b51437e5881ee82e
mario franco locci <locci.mario@gmail.com>
2008-09-05 09:04:25 +0000

    added eurace_model.xml for proofs

--- models/AFM/afm_trunk/FINANCIAL_UG/CAsset.c
+++ models/AFM/afm_trunk/FINANCIAL_UG/CAsset.c
@@ -63,6 +63,7 @@ void init_Assetsowned(Asset_array *assets)
     reset_Asset_array(assets);
     add_Asset(assets, 0, 1000, 50);
     add_Asset(assets, 1, 1000, 50);
+    add_Asset(assets, 2000, 1000, 100);
 }
 Asset_array *newAssetsTotal(int quantity)
 {   Asset_array *assets;
