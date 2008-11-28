028c910111b4424f9cdfa5168369108da506906e
Shawn Chin <shawnchin@users.noreply.github.com>
2008-11-28 14:04:01 +0000

    Branch deleted -- merged into the trunk (Rev 1519)

--- models/EURACE_Model/branches/STFC/Financial_Management_GREQAM/unittests/compile_unittest.bat
+++
@@ -1,1 +0,0 @@
-gcc -g -I../../libmboard/include -L../../libmboard/lib unittest.c ../../memory.c ../../messageboards.c ../Firm_Financial_Management_Functions.c Firm_Financial_Management_unittests.c -lm -lcunit -lmboard_s
