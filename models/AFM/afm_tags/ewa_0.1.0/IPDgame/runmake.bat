002b6362349cd7d2d74c4835cfce7cc55b0e5ca1
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2008-12-03 14:43:59 +0000

    Tag of EWA module (03/12/2008)

---
+++ models/AFM/afm_tags/ewa_0.1.0/IPDgame/runmake.bat
@@ -0,0 +1,4 @@
+PATH=C:\MinGW\bin;D:\Docs\EURACE\X-models\Xparser-0.12.0;%PATH%
+
+mingw32-make main
+pause
---
+++ models/AFM/afm_tags/ewa_0.1.0/IPDgame/some_new_functions.h
@@ -0,0 +1,25 @@
+/************************************
+ * \file  some_new_functions.h
+ * \brief Header file for auxiliary functions.
+ * *********************************
+ * History:
+ * 15/07/08 Sander: General library functions. 
+ *********************************/
+
+//Set debug print outs
+//#define print_debug 1 //now set in header.h as environment variable
+
+/* Statistic auxiliary functions */
+double random_unif();
+double random_unif_interval(double a, double b);
+double max(double a, double b);
+double min(double a, double b);
+
+double sum(double * p, int size);
+void cumsum(double * p, int size, double * cumsum);
+void cumpdf(double * p, int size, double * cpdf);
+int draw(int size, double * cpdf);
+int ismember(int i, int * xvec, int size);
+int ismember_double(double i, double * xvec, int size);
+void draw_without_replacement(int size, double * pdf, int nr_draws, int * draws);
+void draw_with_replacement(int size, double * pdf, int nr_draws, int * draws);
