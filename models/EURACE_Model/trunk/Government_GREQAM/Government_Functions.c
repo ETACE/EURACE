55e224d954d3950c153103179bdb209b89292b5f
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-01-20 13:11:18 +0000

    Bug fixes: Mall, Gov, Eurostat

--- models/EURACE_Model/trunk/Government_GREQAM/Government_Functions.c
+++ models/EURACE_Model/trunk/Government_GREQAM/Government_Functions.c
@@ -7,7 +7,7 @@
 //#include "Government_agent_header.h"
 //#include "../my_library_header.h"
 
-#define NO_REGIONS_PER_GOV 2 //number of regions per gov
+//#define NO_REGIONS_PER_GOV 2 //number of regions per gov
 
 /************Government Role: Finance********************************/
 int Government_idle()
