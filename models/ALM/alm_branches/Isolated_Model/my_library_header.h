a1b6c80f8f763a3926a379d5de31e2184fb2f98a
svdhoog <11057058+svdhoog@users.noreply.github.com>
2009-01-22 15:34:41 +0000

    edits

--- models/ALM/alm_branches/Isolated_Model/my_library_header.h
+++ models/ALM/alm_branches/Isolated_Model/my_library_header.h
@@ -3,14 +3,18 @@
  * \brief Header for user created library functions.
  */
 
+#define DAY iteration_loop
+#define MONTH 20
+
+/*
 int first_day_of_month();
 int not_first_day_of_month();
 int last_day_of_month();
 int not_last_day_of_month();
+*/
 
 void sort_vacancy_list(vacancy_array * vacancy_list);
 void sort_job_offer_list(job_offer_array * job_offer_list);
-int random_int(int min,int max);
 void sort_job_application_list(job_application_array * job_application_list);
 void sort_mall_sales_list(sales_statistics_array * sales_mall_list);
 
@@ -21,5 +25,6 @@ int employee_list_rank_specific_skills_function(const void *x, const void *y);
 int job_application_list_rank_general_skill_function(const void *x, const void *y);
 int sales_statistics_list_rank_sales_function(const void *x, const void *y);
 int random_int(int min,int max);
+
 double max(double a, double b);
 double min(double a, double b);
