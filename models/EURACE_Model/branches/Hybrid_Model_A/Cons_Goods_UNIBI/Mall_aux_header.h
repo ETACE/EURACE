a3631dd8b583a6e0238c6a5ec69836ca04edd1ea
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-01-28 09:52:40 +0000

    splitted my_library_headers into module specific files

--- models/EURACE_Model/branches/Hybrid_Model_A/Cons_Goods_UNIBI/Mall_aux_header.h
+++ models/EURACE_Model/branches/Hybrid_Model_A/Cons_Goods_UNIBI/Mall_aux_header.h
@@ -7,4 +7,6 @@
  *********************************/
 
 /* Mall auxiliary functions */
+void sort_mall_sales_list(sales_statistics_array * sales_mall_list);
+int sales_statistics_list_rank_sales_function(const void *x, const void *y);
 void Mall_add_export_data(int firm_region, int household_region, double export_volume, double export_value, double export_previous_value);
