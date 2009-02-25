b1654282689eab0204f927090c277c771c97c589
pharting <pharting@wiwi.uni-bielefeld.de>
2009-02-25 14:46:38 +0000

    Committing of the unit test files for the firm producer role

--- models/EURACE_Model/branches/Integrated_Model/Cons_Goods_UNIBI/unittests/unittest.c
+++ models/EURACE_Model/branches/Integrated_Model/Cons_Goods_UNIBI/unittests/unittest.c
@@ -3,14 +3,34 @@
  * \brief Holds main function of the unittest program.
  */
 #include "../../header.h"
-//#include "../../header.h"
 #include <CUnit/Basic.h>
 
 /*************************** unittest prototypes ***************************/
 void unittest_Firm_execute_production();
 void unittest_Firm_execute_production_1();
 void unittest_Firm_execute_production_2();
-
+void unittest_Firm_calc_production_quantity_1();
+void unittest_Firm_calc_production_quantity_2();
+void unittest_Firm_calc_production_quantity_3();
+void unittest_Firm_receive_capital_goods();
+void unittest_Firm_calc_pay_costs();
+void unittest_Firm_calc_input_demands();
+void unittest_Firm_calc_input_demands_2();
+void unittest_Firm_calc_input_demands_3();
+void unittest_Firm_calc_input_demands_4();
+void unittest_Firm_calc_input_demands_5();
+void unittest_Firm_calc_production_quantity_II();
+void unittest_Firm_calc_production_quantity_II_2();
+void unittest_Firm_calc_production_quantity_II_3();
+void unittest_Firm_calc_input_demands_II();
+void unittest_Firm_calc_input_demands_II_2();
+void unittest_Firm_send_goods_to_mall();
+void unittest_Firm_send_goods_to_mall_2();
+void unittest_Firm_send_goods_to_mall_3();
+void unittest_Firm_send_goods_to_mall_4();
+void unittest_Firm_calc_revenue();
+void unittest_Firm_compute_sales_statistics();
+void unittest_Firm_update_specific_skills_of_worker();
 /*************************** end prototypes ***************************/
 
 int init_suite1(void)
@@ -50,10 +70,33 @@ int main(int argc, char * argv[])
     /* add the tests to the suite */
     /* add extra tests using || */
     
-    if(
-//   		 NULL == CU_add_test(pSuite, "Firm_execute_production",unittest_Firm_execute_production) ||
-//   		 NULL == CU_add_test(pSuite, "Firm_execute_production",unittest_Firm_execute_production_1)||
-   		 NULL == CU_add_test(pSuite, "Firm_execute_production",unittest_Firm_execute_production_2))
+<<<<<<< .mine
+    if(NULL == CU_add_test(pSuite, "Firm_execute_production Case 1",unittest_Firm_execute_production) ||
+    		NULL == CU_add_test(pSuite, "Firm_execute_production Case 2",unittest_Firm_execute_production_1)||
+    		NULL == CU_add_test(pSuite, "Firm_execute_production Case 3",unittest_Firm_execute_production_2)||
+    		NULL == CU_add_test(pSuite, "Firm_calc_production_quantity(), Case 1",unittest_Firm_calc_production_quantity_1) ||
+    		NULL == CU_add_test(pSuite, "Firm_calc_production_quantity(), Case 2",unittest_Firm_calc_production_quantity_2) || 
+    		NULL == CU_add_test(pSuite, "Firm_calc_production_quantity() Case 3",unittest_Firm_calc_production_quantity_3)||
+    		NULL == CU_add_test(pSuite, "Firm_receive_capital_goods",unittest_Firm_receive_capital_goods)||
+NULL == CU_add_test(pSuite, "Firm_calc_pay_costs()",unittest_Firm_calc_pay_costs)|| 
+NULL == CU_add_test(pSuite, "Firm_calc_input_demands Case 1",unittest_Firm_calc_input_demands)||
+NULL == CU_add_test(pSuite, "Firm_calc_input_demands Case 2",unittest_Firm_calc_input_demands_2)|| 
+NULL == CU_add_test(pSuite, "Firm_calc_input_demands Case 3",unittest_Firm_calc_input_demands_3)||
+NULL == CU_add_test(pSuite, "Firm_calc_input_demands Case 4",unittest_Firm_calc_input_demands_4)|| 
+NULL == CU_add_test(pSuite, "Firm_calc_input_demands Case 5",unittest_Firm_calc_input_demands_5)||
+NULL == CU_add_test(pSuite,  "Firm_calc_production_quantity Case 1",unittest_Firm_calc_production_quantity_II)||
+NULL == CU_add_test(pSuite,  "Firm_calc_production_quantity_2 Case 2",unittest_Firm_calc_production_quantity_II_2)||
+NULL == CU_add_test(pSuite,  "Firm_calc_production_quantity_2 Case 3",unittest_Firm_calc_production_quantity_II_3)||
+NULL == CU_add_test(pSuite,  "Firm_calc_input_demands_2 Case 1",unittest_Firm_calc_input_demands_II)||
+NULL == CU_add_test(pSuite,  "Firm_calc_input_demands_2 Case 2",unittest_Firm_calc_input_demands_II_2)||
+NULL == CU_add_test(pSuite,  "Firm_send_goods_to_mall Case 1",unittest_Firm_send_goods_to_mall)||
+NULL == CU_add_test(pSuite,  "Firm_send_goods_to_mall Case 2",unittest_Firm_send_goods_to_mall_2)||
+NULL == CU_add_test(pSuite,  "Firm_send_goods_to_mall Case 3",unittest_Firm_send_goods_to_mall_3)||
+NULL == CU_add_test(pSuite,  "Firm_send_goods_to_mall Case 4",unittest_Firm_send_goods_to_mall_4)||
+NULL == CU_add_test(pSuite,  "Firm_calc_revenue",unittest_Firm_calc_revenue)||
+NULL == CU_add_test(pSuite,  "Firm_compute_sales_statistics",unittest_Firm_compute_sales_statistics)||
+NULL == CU_add_test(pSuite,  "Firm_update_specific_skills_of_worker",unittest_Firm_update_specific_skills_of_worker))
+
 	{
         CU_cleanup_registry();
         return CU_get_error();
