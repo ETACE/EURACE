4c826049dd8faceb34c95f315d1f5c87d47e8dd2
saul <saul@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-04-01 14:58:28 +0000

    Implemented unique identifiers for banks in ACM; removed useless memory variables

--- models/EURACE_Model/branches/Hybrid_Model_A/Credit_Ancona/Copia di Bank_Credit_Functions.c
+++ models/EURACE_Model/branches/Hybrid_Model_A/Credit_Ancona/Copia di Bank_Credit_Functions.c
@@ -2,15 +2,6 @@
     #include "../Bank_agent_header.h"
     #include "../my_library_header.h"
     
-    int Bank_communicate_identity()
-    {
-        add_bank_identity_message(ID);
-        
-     return 0;   
-    }
-        
-    
-    
     int Bank_decide_credit_conditions()
     {
         double e, c, d, r, i;
