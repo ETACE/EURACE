72707016c96d7d0d11b7c73c3fb537c3eda8252b
svdhoog <11057058+svdhoog@users.noreply.github.com>
2009-05-01 08:47:36 +0000

    added dummy clearinghouse

--- models/EURACE_Model/branches/Hybrid_Model_A/Financial_UNIBI/ClearingHouse_Financial_Market.c
+++ models/EURACE_Model/branches/Hybrid_Model_A/Financial_UNIBI/ClearingHouse_Financial_Market.c
@@ -1,18 +1,18 @@
 #include "../Clearinghouse_agent_header.h"
 //#include "my_library_header.h"
 
-int ClearingHouse_receive_orders_and_run()
+int ClearingHouse_receive_orders_and_run_dummy()
 {  
-	return 0;
+    return 0;
 }
 
 
-int ClearingHouse_send_asset_information()
+int ClearingHouse_send_asset_information_dummy()
 {
-	return 0;
+    return 0;
 }
 
-int ClearingHouse_receive_info()
+int ClearingHouse_receive_info_dummy()
 {
-	return 0;
+    return 0;
 }
