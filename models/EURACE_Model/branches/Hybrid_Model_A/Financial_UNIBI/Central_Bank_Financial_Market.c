18fb76db6b5d0eb24e82c044af90e1fd2b69c5ae
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-05-01 08:47:36 +0000

    added dummy clearinghouse

--- models/EURACE_Model/branches/Hybrid_Model_A/Financial_UNIBI/Central_Bank_Financial_Market.c
+++ models/EURACE_Model/branches/Hybrid_Model_A/Financial_UNIBI/Central_Bank_Financial_Market.c
@@ -1,6 +1,6 @@
 #include "../header.h"
 #include "../Central_Bank_agent_header.h"
-#include "../my_library_header.h"
+//#include "../my_library_header.h"
     
 /************Central_Bank Role: ********************************/
 
