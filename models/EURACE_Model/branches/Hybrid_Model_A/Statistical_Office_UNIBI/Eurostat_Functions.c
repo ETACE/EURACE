0810112834be6c8cadff37cb0e6d94125542026a
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-05-05 16:36:24 +0000

    bugfixes to model.xml with Shawn's consistency script

--- models/EURACE_Model/branches/Hybrid_Model_A/Statistical_Office_UNIBI/Eurostat_Functions.c
+++ models/EURACE_Model/branches/Hybrid_Model_A/Statistical_Office_UNIBI/Eurostat_Functions.c
@@ -19,9 +19,6 @@ int Eurostat_send_data_to_government()
     int i, region;
     double gdp;
     
-    //printf("AVERAGE_WAGE %f\n",AVERAGE_WAGE);
-    //add_mean_wage_for_government_message(1, AVERAGE_WAGE);
-
     //add message for each region
     for (i=0; i<NO_REGIONS; i++)
     {
