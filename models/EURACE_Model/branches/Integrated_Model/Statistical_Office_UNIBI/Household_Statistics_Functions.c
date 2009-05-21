19614214a4f0a5dff8e97146d243de89ff094cd8
svdhoog <11057058+svdhoog@users.noreply.github.com>
2009-05-21 09:38:57 +0000

    Bugfix: HH payment_account update at end of every iteration

--- models/EURACE_Model/branches/Integrated_Model/Statistical_Office_UNIBI/Household_Statistics_Functions.c
+++ models/EURACE_Model/branches/Integrated_Model/Statistical_Office_UNIBI/Household_Statistics_Functions.c
@@ -23,8 +23,9 @@ int Household_send_data_to_Eurostat()
 {   
     add_household_send_data_message(ID, REGION_ID, GENERAL_SKILL,EMPLOYEE_FIRM_ID,
     WAGE, SPECIFIC_SKILL);
-        
-    if (SWITCH_STOCK_CONSISTENCY_CHECK)
+    
+    /*AIX*/
+    if ((SWITCH_FLOW_CONSISTENCY_CHECK)||(SWITCH_STOCK_CONSISTENCY_CHECK))
     {
         //Set these to correct expressions:
         GOV_BOND_HOLDINGS=0.0;
@@ -32,14 +33,15 @@ int Household_send_data_to_Eurostat()
         NR_FIRM_SHARES=0;
         GOV_INTEREST=0.0;
         STOCK_SALES=0.0;
-//      MONTHLY_CONSUMPTION_EXPENDITURE=0.0;
+        MONTHLY_CONSUMPTION_EXPENDITURE=0.0;
         STOCK_PURCHASES=0.0;
         TOTAL_ASSETS=0.0;
         TOTAL_LIABILITIES=0.0;
         TOTAL_INCOME=0.0;
         TOTAL_EXPENSES=0.0;
 
-        add_household_balance_sheet_message(PAYMENT_ACCOUNT, GOV_BOND_HOLDINGS, NR_GOV_BONDS, NR_FIRM_SHARES,
+        //printf("\n Household %d: my WAGE=%2.2f.\n", ID, WAGE);
+        add_household_balance_sheet_message(PAYMENT_ACCOUNT, GOV_BOND_HOLDINGS, NR_GOV_BONDS, NR_FIRM_SHARES, 
                 WAGE, GOV_INTEREST, STOCK_SALES,
                 CUM_TOTAL_DIVIDENDS, MONTHLY_CONSUMPTION_EXPENDITURE, TAX_PAYMENT, STOCK_PURCHASES, 
                 TOTAL_ASSETS, TOTAL_LIABILITIES, TOTAL_INCOME, TOTAL_EXPENSES);
