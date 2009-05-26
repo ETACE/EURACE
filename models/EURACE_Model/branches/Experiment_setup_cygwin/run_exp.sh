6c37937ec6a1a06afc42591be3da4be53d835d5f
svdhoog <11057058+svdhoog@users.noreply.github.com>
2009-05-26 14:13:01 +0000

    Scripts for experiments

---
+++ models/EURACE_Model/branches/Experiment_setup_cygwin/run_exp.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+################################################################################################################
+# This script is for setting up and running the experiment:
+# Steps:
+# - 1 : experiment settings
+# - 2 : creation of experiment folder hierarchy
+# - 3 : creation of the specific settings xml file
+# - 4 : running the initial transient phase; stores a snapshot of the last iteration in transient.xml
+# - 5 : running the experiment
+################################################################################################################
+
+BASE=$PWD
+F1="duration_40 duration_120 duration_240 duration_560"
+F2="intensity_1 intensity_5 intensity_10 intensity_20"
+F3="frequency_0 frequency_20 frequency_40 frequency_60"
+
+echo 'Starting top-level experiment script...'
+
+######### STEP 1: EXPERIMENT SETTINGS #########################################################################
+
+#Construct the RUNS list:
+TOTAL_RUNS=4;
+RUNS=''
+i=0
+for ((j=0; j<TOTAL_RUNS; j++)); do
+    RUNS=$RUNS' '$j
+done
+
+echo '  Number of batch runs:' $TOTAL_RUNS
+
+######### STEP 2: CREATION OF EXPERIMENT FOLDER HIERARCHY #####################################################
+sh exp_script_1.sh
+
+######### STEP 3: CREATION OF THE SPECIFIC SETTINGS XML FILE ##################################################
+#sh exp_script_2.sh
+
+######### STEP 4: RUNNING THE INITIAL PHASE ###################################################################
+#sh run_first_stage.sh
+
+######### STEP 5: RUNNING THE EXPERIMENT ######################################################################
+sh run_experiment.sh
+
+echo 'Finished top-level experiment script.'
---
+++ models/EURACE_Model/branches/Experiment_setup_linux/run_first_stage.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+######### RUNNING THE INITIAL PHASE ###############################################################
+# Script to run the first stage simulation:
+# Runs the transient phase, and then saves the final iteration as transient.xml in the top folder
+# Steps:
+# - run in the top-level folder: ./its/0.xml (see output_last.xml)
+# - save a full snapshot in the last iteration only (see output_last.xml)
+# - join the node-*.xml files to a single file
+# - the final xml is renamed to transient.xml
+###################################################################################################
+# Usage:
+# - NR_NODES: number of nodes to use
+# - ITS: number of iterations
+# - MAIN: location of executable file
+# - run
+# - remove
+
+echo '  Starting first stage of experiment...'
+
+NR_NODES=4
+ITS=500
+MAIN='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/branches/Hybrid_Model_A_energy_shock/main'
+
+echo '      Starting run for initial transient...'
+#mpiexec -n $NR_NODES $MAIN $ITS output_last.xml
+echo '      Finished run for initial transient.'
+
+#Concatenating the node files
+echo '      Starting concatenation of the node output files'
+#sh ~/cloning/join.sh ./ $NR_NODES
+echo '      Finished concatenation'
+
+#Removing the node files
+rm -f node*.xml
+echo '      Removed node output files'
+
+#Rename the $ITS.xml to transient.xml
+#mv $ITS.xml transient.xml
+echo '      Renamed last iteration xml file to transient.xml'
+
+echo '  Finished first stage of experiment.'
