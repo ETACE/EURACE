ad10b96c96571e6e275e77d4b6350eaa990340ec
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-05-26 12:40:01 +0000

    bugfix: fiat money loophole Gov -> ECB

--- models/EURACE_Model/branches/Integrated_Model/Credit_Ancona/Central_Bank_Functions.c
+++ models/EURACE_Model/branches/Integrated_Model/Credit_Ancona/Central_Bank_Functions.c
@@ -8,6 +8,29 @@
 /** \fn Central_Bank_read_account_update()
  * \brief 
  */
+int Central_Bank_read_fiat_money_requests()
+{
+
+    //Read the bond emission -> fiat money request from governments
+    START_ISSUE_BONDS_TO_ECB_MESSAGE_LOOP
+        FIAT_MONEY += issue_bonds_to_ecb_message->nominal_value;
+        //BOND_HOLDINGS += issue_bonds_to_ecb_message->quantity;
+    FINISH_ISSUE_BONDS_TO_ECB_MESSAGE_LOOP
+
+
+    //Read the fiat money request from governments
+    //request_fiat_money_message(nominal_value);
+    START_REQUEST_FIAT_MONEY_MESSAGE_LOOP
+        FIAT_MONEY += request_fiat_money_message->nominal_value;
+    FINISH_REQUEST_FIAT_MONEY_MESSAGE_LOOP
+
+
+    return 0;
+}
+
+/** \fn Central_Bank_read_account_update()
+ * \brief 
+ */
 int Central_Bank_read_account_update()
 {
     int i;
