40975ef054143bde70b6d58a12a0730bf8795580
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-05-27 17:49:20 +0000

    update of Financial_UNIBI: - removed all financial market functions of gov and ECB Reason: Government_GREQAM in Integrated Model is updated, so no need for dummies here

--- models/EURACE_Model/branches/Hybrid_Model_A/Financial_UNIBI/Central_Bank_Financial_Market.c
+++ models/EURACE_Model/branches/Hybrid_Model_A/Financial_UNIBI/Central_Bank_Financial_Market.c
@@ -3,21 +3,3 @@
 //#include "../my_library_header.h"
     
 /************Central_Bank Role: ********************************/
-
-/** \fn Central_Bank_read_bond_update()
- * \brief Read bond messages from Governments
- */
-int Central_Bank_read_bond_update()
-{
-    GOV_BOND_PURCHASE=0.0;
-    
-    START_GOV_BOND_MESSAGE_LOOP
-        GOV_BOND_PURCHASE += gov_bond_message->amount;
-    FINISH_GOV_BOND_MESSAGE_LOOP
-    
-    //Update stock variables
-    GOV_BOND_HOLDINGS += GOV_BOND_PURCHASE;
-    FIAT_MONEY += GOV_BOND_PURCHASE;
-    
-    return 0;
-} 
