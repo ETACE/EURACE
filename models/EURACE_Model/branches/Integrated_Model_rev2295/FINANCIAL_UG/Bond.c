1c3250413fdae713b6f2906478240e3d3d5357d9
svdhoog <11057058+svdhoog@users.noreply.github.com>
2009-06-19 09:02:55 +0000

    Branch of rev 2295 at end of Genoa June Meeting; need to change Financial_UG to rev 2282 for this to work

---
+++ models/EURACE_Model/branches/Integrated_Model_rev2295/FINANCIAL_UG/Bond.c
@@ -0,0 +1,125 @@
+#include "my_library_header.h"
+void initializeBond(Bond *aBond,int id, int nr_outstanding,double face_value, double nominal_yield)
+{ int i; 
+  double aux;
+  double prec;
+  aBond->id=id;
+  aBond->index=0;
+   aBond->nr_outstanding=nr_outstanding;
+  aBond->face_value=face_value;
+  aBond->nominal_yield=nominal_yield;
+  aBond->maturity_day=10000;
+  aBond->issue_day=0;
+  aux=face_value;
+
+ for(i=0;i<MAXPRICES;i++)
+     { prec=aux;
+      aux=prec+0.01*gauss(0,0.1);
+      addPriceBond(aBond,aux);
+     }
+}
+double  backreturns_bond(Bond *bond,int ind)
+    { int index;
+      index=bond->index-ind;
+      if(index<0) index=MAXRETURNS+index;
+        
+       return bond->returns[index];
+        
+   }
+double lastPriceBond(Bond *aBond)
+{       
+        double answer;
+        answer=aBond->prices[aBond->index];
+        return answer;
+}
+
+
+void addPriceBond(Bond *bond, double price)
+{    int precindex;
+     precindex=bond->index;
+     if(bond->index==MAXPRICES-1) bond->index=0;
+                                    
+     else bond->index++;
+     bond->prices[bond->index]=price;
+     bond->returns[bond->index]= (price-bond->prices[precindex])/price;
+  
+}
+double expectedReturnBond(Bond *bond,int backwardWindow)
+       { double mean;
+         int i;
+         mean=0;
+         for(i=0;i<backwardWindow;i++)
+             mean=mean+backreturns_bond(bond,i);
+         mean=mean/(backwardWindow);
+         return mean;
+       }
+
+     
+double volatilityBond(Bond *bond,int backwardWindow)
+     { double expectedreturn;
+       double volatility;
+       double aux;
+       int i;
+       volatility=0;
+ 
+       expectedreturn=expectedReturnBond(bond,backwardWindow);
+        for(i=0;i<backwardWindow;i++)
+            {  
+               aux=(backreturns_bond(bond,i)-expectedreturn);
+               volatility=volatility+(aux*aux);
+            }
+      volatility=volatility/(backwardWindow);
+      volatility=sqrt(volatility);
+      return volatility;
+     }
+void totalReturnsBond(Bond *bond, double *vect, int backwardWindow,double factor, double value)
+  {  int i;
+     for(i=0;i<(backwardWindow);i++)
+     vect[i]=backreturns_bond(bond,i)*factor+value;
+  }
+void priceReturnsBond(Bond *bond, double *vect, int backwardWindow)
+  {  int i;
+    
+     for(i=0;i<(backwardWindow);i++)
+     vect[i]=backreturns_bond(bond,i);
+  }
+
+
+
+void historicalReturnsBond(Bond *bond, double *vect, int backwardWindow,double forwardWindow)
+  {  int index,i;
+     double *returns;
+     index=bond->index;
+     returns=bond->returns;
+     for(i=0;i<(backwardWindow);i++)
+     vect[i]=backreturns_bond(bond,i)*forwardWindow;
+  }
+/**/
+int  coupons_payment_days(Bond *bond,int currentDay,int holding_period)
+{ 
+   int nrCoupons;
+   int coupon_day_period;
+   int issue_day;
+   issue_day=bond->issue_day;
+   coupon_day_period=(int)DAYS_IN_MONTH*COUPONPERIODICITYNRMONTHS;
+   nrCoupons= ((currentDay+issue_day+holding_period)/coupon_day_period)-(currentDay+issue_day)/coupon_day_period;
+   return nrCoupons;
+}
+
+double computeBondUtilityFunction(Bond *bond,int backwardWindow, double factor, double value, double lossaversion,double *rndvect, double randomWeight)
+{ 
+  int i;
+  double utility;
+  double r;
+  utility=0;
+  for(i=0;i<backwardWindow;i++)
+    { 
+     r=backreturns_bond(bond,i)*factor+value+rndvect[i]*randomWeight;
+     
+     if (r<0) utility=utility+r*lossaversion;
+     else     utility=utility+r;
+    }
+  return utility/backwardWindow;
+
+}
+
---
+++ models/EURACE_Model/branches/Integrated_Model_rev2295/FINANCIAL_UG/Government_Financial_Market.c
@@ -0,0 +1,136 @@
+#include "../Government_agent_header.h"
+#include "my_library_header.h"
+//#include "../header.h"
+
+
+int Government_BondIssuing_decision()
+{ int GovBondNewIssueAmount;
+  Bond *bond;
+  double limit_price,last_market_price;
+  bond=get_bond();
+  if(bond->maturity_day>CURRENTDAY) last_market_price=lastPriceBond(bond);
+  limit_price = (1-BONDS_NEWISSUE_DISCOUNT)*last_market_price;
+ GovBondNewIssueAmount = ceil(TOTAL_BOND_FINANCING/limit_price);
+
+  return GovBondNewIssueAmount;
+}
+
+int Government_send_info_bond(void)
+{    Bond *bond;
+     bond=get_bond();
+     add_info_bond_message(*bond);
+     return 0;
+}
+
+int Government_orders_issuing(void)
+    {  Order *pending_order;
+       Bond *bond;
+       int issuer;
+       pending_order=get_pending_order();
+       issuer=get_id();
+       bond=get_bond();
+       if(pending_order->quantity!=0)
+        {
+         //printf("invio ordini di vendita bond id%d price%f quantity%d",bond->id, pending_order->price,pending_order->quantity);
+         add_order_message(issuer,bond->id, pending_order->price,pending_order->quantity);
+        }
+    return 0;
+   }
+int Government_generate_pending_orders(void)
+    {int  qty,issuer;
+     double  GovBondsInPortfolioValue,lastMarketPrice,govdeficit; 
+     int GovBondNewIssueAmount;
+     Bond *bond;
+     Order *pending_order;
+      double limitPrice;
+     double payment_account;
+     bond=get_bond();
+     pending_order=get_pending_order();
+     issuer=get_id();
+     payment_account=get_payment_account();
+     qty =bond->quantity;
+     lastMarketPrice = lastPriceBond(bond);
+     GovBondsInPortfolioValue =  qty*lastMarketPrice;
+   // printf("BONDS_NEWISSUE_DISCOUNT==%f",BONDS_NEWISSUE_DISCOUNT);
+if ((payment_account+GovBondsInPortfolioValue)<0)
+   {
+    govdeficit=(payment_account+GovBondsInPortfolioValue);
+    //set_deficit(govdeficit);
+    GovBondNewIssueAmount=Government_BondIssuing_decision();//govdeficit);
+    bond->quantity = bond->quantity + GovBondNewIssueAmount;
+    //bond->nr_outstanding= bond->nr_outstanding +GovBondNewIssueAmount;
+   //printf("BONDS_NEWISSUE_DISCOUNT==%f",BONDS_NEWISSUE_DISCOUNT);
+    limitPrice = (1- BONDS_NEWISSUE_DISCOUNT)*lastMarketPrice;
+   //printf(" Government_generate_pending_orders id%d price%f quantity%d",ID, limitPrice,GovBondNewIssueAmount);
+    qty =-bond->quantity;
+    pending_order->quantity= qty;
+    pending_order->price=limitPrice;
+    pending_order->issuer=ID;
+    pending_order->assetId=bond->id;
+ }
+return 0;
+}
+
+int Government_update_its_portfolio()
+{ int issuer;
+  double payment_account;
+  Bond *bond;
+  Order *pending_order;
+  m_order_status *order_status;
+  int sold_quantity;
+  pending_order=get_pending_order();
+  payment_account=get_payment_account();
+  bond=get_bond();
+  issuer=get_id();
+  sold_quantity=0;
+  if(pending_order->quantity!=0)
+  {
+  order_status=get_first_order_status_message();
+  while(order_status)
+  { if(order_status->trader_id==issuer) 
+      { 
+       pending_order->quantity=pending_order->quantity-order_status->quantity;
+       payment_account=payment_account-order_status->price*order_status->quantity;
+       set_payment_account(payment_account);
+       bond->quantity=bond->quantity+order_status->quantity;
+       sold_quantity=sold_quantity-order_status->quantity;
+       }
+
+     order_status=get_next_order_status_message(order_status);   
+     
+  }
+  }
+  bond->nr_outstanding = bond->nr_outstanding +sold_quantity;
+ // if(i==2) printf("numero di execuzione =%d\n",i);
+ //printf("numero di outstanding bonds =%d\n",bond->nr_outstanding);
+  return 0;
+}
+
+int Government_receive_info_bond(void)
+{ Bond *bond;
+   double price;
+   bond =get_bond();
+  
+   m_infoAssetCH  *current;
+   current=get_first_infoAssetCH_message();
+   
+ while(current)
+  {
+   
+   if(ID==current->asset_id) 
+       {  price=current->price;
+          addPriceBond(bond,price);
+       }
+    current=get_next_infoAssetCH_message(current);
+   }
+  return 0;
+}
+
+int Government_pays_coupons()
+   { double coupon;
+     coupon=BOND.nominal_yield*BOND.face_value;
+     PAYMENT_ACCOUNT=PAYMENT_ACCOUNT-(coupon*BOND.nr_outstanding);
+     MONTHLY_BOND_INTEREST_PAYMENT = coupon*BOND.nr_outstanding; //Defined in Government/model.xml Gov memory
+     add_payment_coupons_message(coupon,ID);
+     return 0;
+   }
