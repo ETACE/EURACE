8bfd01c2a3727b956ae57ab3473446c0b580ba4a
smg <sgemkow@wiwi.uni-bielefeld.de>
2009-06-25 11:56:32 +0000

    added region_ids to several datatypes

--- models/ALM/alm_branches/Isolated_Model_UNIBI_May09/create2/create.c
+++ models/ALM/alm_branches/Isolated_Model_UNIBI_May09/create2/create.c
@@ -197,12 +197,12 @@ double SUBSIDY_FRACTION = 0.0;
 	double 	payment_account_household = 0;
 
 	double   cap_price; // temp variable for printing capital_good_price_region into memory variables of the firm
-	double	capital_good_price = 2;
-	double	capital_good_price_region[2][1] = {2.0,2.0};
+	double	capital_good_price = 6;
+	double	capital_good_price_region[2][1] = {6.0,2.0};
 	
-	double productivity_best_practice_igfirm = 1.1;  //Productivity of the technology offered by the IG firm
+	double productivity_best_practice_igfirm = 3.3;  //Productivity of the technology offered by the IG firm
 	double productivity_best_practice[2][1]=
-					{1.1,1.1};
+					{3.3,1.1};
 
 
 	int years_statistics = 10;/*number of years used to smooth the production*/
@@ -288,11 +288,11 @@ double SUBSIDY_FRACTION = 0.0;
 	
 	//This determines the local skill distribution. Use numbers 1 .. 4 (see above, skill distribution_X) to give a region a skill distribution. 
 		int skills_in_regions[2][1]=
-					{4,4};
+					{4,3};
 	
 	
 		//region specific initial value of households specific skills
-		double spec_s_hh_reg1 = 1.0;
+		double spec_s_hh_reg1 = 3.0;
 		double spec_s_hh_reg2 = 1.0;
 		double specific_skills_of_household[2][1]=
 							{spec_s_hh_reg1,spec_s_hh_reg2};
@@ -300,13 +300,13 @@ double SUBSIDY_FRACTION = 0.0;
 
 		//Total production volume for a single firm
 			double total_production_quantity[2][1]=
-						{27.0,27.0};
+						{81.0,27.0};
 			//This defines the initial capital stock of firm depending on the region.
 			double total_units_capital[2][1]=
 				                            {45.0,45.0};
 			//Firm's starting value of productivity of the capital stock
 			double technology[2][1]=
-							{1.0,1.0};
+							{3.0,1.0};
 
 			//This defines the financial resources of firm at the beginning of a simulation
 			double payment_account[2][1]= 
