6feff44e9449888d29669bd202a71df29d0c7f41
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-07-15 09:47:00 +0000

    removed old sort function (was commented out anyway)

--- models/EURACE_Model/branches/Integrated_Model_rev2295/my_library_functions.c
+++ models/EURACE_Model/branches/Integrated_Model_rev2295/my_library_functions.c
@@ -6,21 +6,6 @@
 #include "header.h"
 #include "my_library_header.h"
 
-
-/*New sorting algorithm from Simon C 3.4.2008*/
-
-/*
-int credit_request_function(const void *x, const void *y)
-{
-if( ((credit_request *)x)->prob_of_failure < ((credit_request *)y)->prob_of_failure) return -1;
-    else if( ((credit_request *)x)->prob_of_failure > ((credit_request *)y)->prob_of_failure)   return 1;
-else return 0;
-
-
-}
-*/
-
-
 /** \fn int random_int(int min,int max)
  * \brief Returns an integer between and including min and max
  * \param min The minimum integer.
