ff23a4dcd5460826eae819fa26636d7d10036775
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-07-20 17:34:33 +0000

    bugfix: missing mesg in xxml

--- models/EURACE_Model/branches/Integrated_Model_rev2295/Credit_Ancona/Central_Bank_Functions.c
+++ models/EURACE_Model/branches/Integrated_Model_rev2295/Credit_Ancona/Central_Bank_Functions.c
@@ -85,7 +85,7 @@ int Central_Bank_read_account_update()
             if(bank_mesg_count!=ACCOUNTS_BANKS.size)
             {    
                 printf("\nERROR in Central_Bank_Functions.c, line 86: nr of mesg not equal to size of bank account array.\n");
-                printf("\n nr of Bank_mesg=%d, size of bank account array=%d\n", bank_mesg_count, ACCOUNTS_BANKS.size);
+                printf("\n Nr of Bank_mesg=%d, size of bank account array=%d\n", bank_mesg_count, ACCOUNTS_BANKS.size);
             }    
             else
                 printf("\n Nr of mesg equal to size of bank account array.\n");
@@ -114,7 +114,7 @@ int Central_Bank_read_account_update()
             if(gov_mesg_count!=ACCOUNTS_GOVS.size)
             {
                 printf("\nERROR in Central_Bank_Functions.c, line 107: nr of mesg not equal to size of gov account array.\n");
-                printf("\n Nr of Gov_mesg=%d, size of bank account array=%d\n", bank_mesg_count, ACCOUNTS_GOVS.size);
+                printf("\n Nr of Gov_mesg=%d, size of gov account array=%d\n", gov_mesg_count, ACCOUNTS_GOVS.size);
             }
             else
                 printf("\n Nr of mesg equal to size of gov account array.\n");
