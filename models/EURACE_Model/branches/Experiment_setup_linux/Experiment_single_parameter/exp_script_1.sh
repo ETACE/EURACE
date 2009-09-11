dc962049b10ccfe403c2a67a51b89ddf68c83cbb
svdhoog <11057058+svdhoog@users.noreply.github.com>
2009-09-11 16:27:09 +0000

    Adapted scripts for multi-jobs

--- models/EURACE_Model/branches/Experiment_setup_linux/Experiment_single_parameter/exp_script_1.sh
+++ models/EURACE_Model/branches/Experiment_setup_linux/Experiment_single_parameter/exp_script_1.sh
@@ -7,12 +7,8 @@
 # - Copy output.xml and run.sh from top-level to these run folders
 ########################################################################################
 
-
-#F1="tax_0.20 tax_0.30 tax_0.50"
-echo $RUNS
-
 echo '  Creating folder hierarchy...'
-echo $F1
+echo '  [' $F1 ']'
 
 rm -f STATUS
 mkdir -p 'its'
@@ -22,11 +18,10 @@ for folder1 in $F1; do
     mkdir -p $folder1
     echo '    Created folder:' $folder1
     cd $folder1
-            echo '        Created folder:' $folder1
             for run in $RUNS; do
                 mkdir -p 'its_'$run
                 echo '          Created folder:' $folder1/'its_'$run
-                echo $folder1'/''run_'$run':CREATED'>> $BASE/STATUS
+                echo $folder1/run_$run:CREATED>> $BASE/STATUS
                 cp $BASE/output.xml $BASE/run.sh ./'its_'$run
     done
     cd ..
@@ -34,3 +29,4 @@ done
 cd ..
 
 echo '  Finished creation of folder hierarchy.'
+echo '  Returned to:'$PWD
--- models/EURACE_Model/branches/Experiment_setup_linux/Experiment_single_parameter/run_exp.sh
+++ models/EURACE_Model/branches/Experiment_setup_linux/Experiment_single_parameter/run_exp.sh
@@ -1,69 +1,90 @@
 #!/bin/bash
 
 ################################################################################################################
-# This script is for exportting up and running the experiment:
+# This script is for setting up and running the experiment:
 # Steps:
-# - 1 : experiment exporttings
+# - 1 : experiment settings
 # - 2 : creation of experiment folder hierarchy
-# - 3 : creation of the specific exporttings xml file
+# - 3 : creation of the specific settings xml file
 # - 4 : running the initial transient phase; stores a snapshot of the last iteration in transient.xml
 # - 5 : running the experiment
 ################################################################################################################
 
 echo 'Starting top-level experiment script...'
 
-######### STEP 1: EXPERIMENT exportTINGS ##################################################################
-env -u NR_NODES
-env -u ITS_PRE
-env -u ITS
-env -u TOTAL_RUNS
-env -u RUNS
-env -u F1
-env -u F1_values
-env -u MAIN_P
-env -u MAIN_S
-env -u BASE
+######### STEP 1: EXPERIMENT SETTINGS ##################################################################
+#env -u NR_NODES
+#env -u ITS_PRE
+#env -u ITS
+#env -u TOTAL_RUNS
+#env -u RUNS
+#env -u F1
+#env -u F1_values
+#env -u MAIN_P
+#env -u MAIN_S
+#env -u BASE
 
 #Set the base folder
 export BASE=$PWD
 
 #Iterations
-export NR_NODES=4
 export ITS_PRE=540
 export ITS=1000
 
-#Runs
-export TOTAL_RUNS=1
+#Set number of nodes to use
+export NR_NODES=4
+
+#Set number of job processes to use
+export NUM_PROCS=4
+
+
+#Set number of batch runs
+export TOTAL_RUNS=4
 RUNS=''
 for ((j=0; j<TOTAL_RUNS; j++)); do
     export RUNS=$RUNS' '$j
 done
-echo $RUNS
+echo 'Batch runs:[' $RUNS ']'
 
 #Parameters
 export F1="tax_0.01 tax_0.05 tax_0.10"
-export F1_values="0.20 0.30 0.50"
+export F1_values="0.01 0.05 0.10"
 
-#Executable
+#Executables
+
+##AIX settings:
 #export MAIN_P='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/trunk/Integrated_Model_1.0/main_p'
 #export MAIN_S='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/trunk/Integrated_Model_1.0/main_s'
-export MAIN_P='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/branches/GREQAM/Integrated_Model_1.0/main_p'
+#export MAIN_P='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/branches/GREQAM/Integrated_Model_1.0/main_p'
 #export MAIN='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/branches/Integrated_Model_1.0_energy_shock/main'
 
+##UNIBI settings:
+export MAIN_S='/home/svdhoog/EURACE/SVN/eurace/branches/GREQAM/Integrated_Model_1.0/main_s'
+export MAIN_P='/home/svdhoog/EURACE/SVN/eurace/branches/GREQAM/Integrated_Model_1.0/main_p'
+#export MAIN_S='/home/svdhoog/EURACE/SVN/eurace/trunk/Integrated_Model_1.0/main_s'
+#export MAIN_P='/home/svdhoog/EURACE/SVN/eurace/trunk/Integrated_Model_1.0/main_p'
 
-######### STEP 2: CREATION OF EXPERIMENT FOLDER HIERARCHY #####################################################
-#bash ./exp_script_1.sh
 
-######### STEP 3: CREATION OF THE SPECIFIC exportTINGS XML FILE ##################################################
-#bash ./exp_script_2.sh
+######### STEP 2: CREATION OF EXPERIMENT FOLDER HIERARCHY 
+bash ./exp_script_1.sh
 
-######### STEP 4: RUNNING THE INITIAL PHASE ###################################################################
+######### STEP 3: CREATION OF THE SPECIFIC SETTINGS XML FILE 
+bash ./exp_script_2.sh
+
+######### STEP 4: RUNNING THE INITIAL PHASE 
 #bash ./run_first_stage.sh
 
-######### STEP 4b: RUNNING BENCHMARK SCENARIO ###################################################################
-bash ./run_benchmark.sh
+######### STEP 4b: RUNNING BENCHMARK SCENARIO 
+#bash ./run_benchmark.sh
 
-######### STEP 5: RUNNING THE EXPERIMENT ######################################################################
+######### STEP 5: RUNNING THE EXPERIMENT 
 #bash ./run_experiment.sh
+
+######### STEP 6: CREATING  JOB SCRIPTS 
+bash ./create_job_list.sh
+
+######### STEP 7: LAUNCHING  JOB SCRIPTS 
+bash ./launch_job_list.sh
+
 
 echo 'Finished top-level experiment script.'
