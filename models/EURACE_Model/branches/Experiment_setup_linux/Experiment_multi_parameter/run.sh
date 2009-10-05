f83b872fdfb05a04b8de9bfd895ed4e444ed88c5
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-10-05 18:11:06 +0000

    update with aix settings

--- models/EURACE_Model/branches/Experiment_setup_linux/Experiment_multi_parameter/run.sh
+++ models/EURACE_Model/branches/Experiment_setup_linux/Experiment_multi_parameter/run.sh
@@ -1,3 +1,5 @@
+
+
 #!/bin/bash
 
 ######### BASE RUN ################################################################
@@ -12,12 +14,21 @@
 # - remove the node-*.xml files
 ###################################################################################
 
-$MAIN_S $ITS output.xml
-#mpiexec -n $NR_NODES $MAIN_P $ITS output.xml -r
-#bash $BASE/join.sh ./ $NR_NODES
-#rm node*.xml
+	$MAIN_S $ITS output.xml
+	#mpiexec -n $NR_NODES $MAIN_P $ITS output.xml -r
+	#bash $BASE/join.sh ./ $NR_NODES
+	#rm node*.xml
 
-tar -cj --remove-files --overwrite --file=xmlfiles.tar.bz *
+	#tar -cj --remove-files --overwrite --file=xmlfiles.tar.bz *
+
+	#Create the SQL database
+	#python $BASE/gendb.py $MODEL_XML_DIR/eurace_model.xml ./
+
+	#Rename to VisGUI default name
+	#mv iterdata.db iters.db
+
+	#Compress the database
+	#tar -cj --remove-files --file=iters.tar.gz iters.db
 
 echo 'Run '$PWD' --DONE'
 echo 'Run '$PWD':DONE' >>$BASE/STATUS 
