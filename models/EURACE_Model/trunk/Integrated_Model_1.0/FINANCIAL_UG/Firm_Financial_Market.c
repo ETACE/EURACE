fb2d2c53b0db5bcea4710e0235356a7ef48ddfbb
svdhoog <11057058+svdhoog@users.noreply.github.com>
2009-10-14 17:01:20 +0000

    Firm_Financial_Market.c, line 17-29: put a print behind debug

--- models/EURACE_Model/trunk/Integrated_Model_1.0/FINANCIAL_UG/Firm_Financial_Market.c
+++ models/EURACE_Model/trunk/Integrated_Model_1.0/FINANCIAL_UG/Firm_Financial_Market.c
@@ -13,14 +13,20 @@ int Firm_send_info(void)
      dividend=CURRENT_DIVIDEND_PER_SHARE;
      earnings_exp = EARNINGS;
      earnings_payout_exp = TOTAL_DIVIDEND_PAYMENT;
-     
-     if(ACTIVE==0)
+
+     //#ifndef _DEBUG_MODE
+     if (PRINT_DEBUG)
      {
-        printf("\n ID=%d, earnings_exp=%f,  dividend=%f, earnings_payout_exp=%f,  EQUITY=%f\n",ID, earnings_exp,  dividend, earnings_payout_exp,  EQUITY);
-        
-        if (EQUITY<0.0)
-            printf("\n In Firm_send_info: EQUITY NEGATIVE\n");
-     }
+     
+		 if(ACTIVE==0)
+		 {
+				printf("\n ID=%d, earnings_exp=%f,  dividend=%f, earnings_payout_exp=%f,  EQUITY=%f\n",ID, earnings_exp,  dividend, earnings_payout_exp,  EQUITY);
+				
+				if (EQUITY<0.0)
+				    printf("\n In Firm_send_info: EQUITY NEGATIVE\n");
+		 }
+	}
+	//#endif
      
      add_info_firm_message(ID, earnings_exp,  dividend, earnings_payout_exp,  EQUITY, STOCK);
      
