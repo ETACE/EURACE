55e999a87b064b976cb295f89b374ef258528251
pharting <pharting@wiwi.uni-bielefeld.de>
2009-11-05 13:14:54 +0000

    Clean up: removed some printfs

--- models/EURACE_Model/trunk/Integrated_Model_1.0/Statistical_Office_UNIBI/Eurostat_aux_functions.c
+++ models/EURACE_Model/trunk/Integrated_Model_1.0/Statistical_Office_UNIBI/Eurostat_aux_functions.c
@@ -21,18 +21,11 @@
 void Eurostat_reset_data(void)
 {
     int i;
- double mean_specific_skills;
 
     /*delete the content of the data arrays in order to store the data for the new month*/
     //free(REGION_HOUSEHOLD_DATA);
     //free(REGION_FIRM_DATA);
     
-    
-	/*Save the mean specific skills from REGION_HOUSEHOLD_DATA in a temproray memory variable in order to have an initial value for the reconstruction of the array*/
-
-	mean_specific_skills = REGION_HOUSEHOLD_DATA.array[0].average_s_skill;
-
-
     //Remove all data arrays
     for(i = 0; i < REGION_FIRM_DATA.size; i++)
     {
@@ -68,7 +61,7 @@ void Eurostat_reset_data(void)
                 0,
                 0.0,0.0,0.0,0.0,0.0,0.0,
                 0.0,0.0,0.0,0.0,0.0,0.0,
-                mean_specific_skills,mean_specific_skills,mean_specific_skills,mean_specific_skills,mean_specific_skills,mean_specific_skills);
+                0.0,0.0,0.0,0.0,0.0,0.0);
     }
     
 }
@@ -308,7 +301,7 @@ void Eurostat_compute_region_firm_data(void)
             REGION_FIRM_DATA.array[i].productivity_progress =
             REGION_FIRM_DATA.array[i].productivity_progress/
             REGION_FIRM_DATA.array[i].no_firms;
-            printf("region: %d  REGION_FIRM_DATA.array[i].productivity_progress %d",i,REGION_FIRM_DATA.array[i].no_firms);
+           
             REGION_FIRM_DATA.array[i].productivity =
             REGION_FIRM_DATA.array[i].productivity/
             REGION_FIRM_DATA.array[i].no_firms;
