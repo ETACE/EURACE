444db8e60b3f1a79f4ecfbbc53dbf616a5dffe5c
svdhoog <11057058+svdhoog@users.noreply.github.com>
2009-11-27 15:27:17 +0000

    updates

--- models/EURACE_Model/branches/Experiment_setup_linux/Experiment_single_parameter/run_exp.sh
+++ models/EURACE_Model/branches/Experiment_setup_linux/Experiment_single_parameter/run_exp.sh
@@ -19,10 +19,10 @@ export BASE=$PWD
 
 #Iterations
 export ITS_PRE=1000
-export ITS=8000
+export ITS=6000
 
 #Set number of nodes to use
-export NR_NODES=2
+export NR_NODES=1
 
 #Set number of job processes to use
 export NUM_PROCS=5
@@ -37,15 +37,23 @@ done
 echo 'Batch runs:[' $RUNS ']'
 
 #Parameters
-export F1="tax_0.0 tax_0.01 tax_0.02 tax_0.03 tax_0.04 tax_0.05 tax_0.06 tax_0.07"
-export F1_values="0.0 0.01 0.02 0.03 0.04 0.05 0.06 0.07"
+#export F1="tax_0.02 tax_0.04 tax_0.06 tax_0.08 tax_0.10"
+#export F1_values="0.02 0.04 0.06 0.08 0.10"
 
+#Tax experiment
 #export F1="tax_0.10 tax_0.15 tax_0.20 tax_0.25"
 #export F1_values="0.10 0.15 0.20 0.25"
-#export F1="tax_0.04"
-#export F1_values="0.04"
+export F1="tax_0.08"
+export F1_values="0.08"
 #export F1="tax_0.05 tax_0.10"
 #export F1_values="0.05 0.10"
+
+#Unemployment benefit experiment:
+export F1="ub_0.60 ub_0.70 ub_0.80 ub_0.90 ub_1.00 ub_1.10 ub_1.20"
+export F1_values="0.60 0.70 0.80 0.90 1.00 1.10 1.20"
+#export F1="ub_0.50"
+#export F1_values="0.50"
+
 
 #export F1="bench"
 #export F1_values=""
@@ -62,17 +70,16 @@ export F1_values="0.0 0.01 0.02 0.03 0.04 0.05 0.06 0.07"
 #export MAIN_P='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/trunk/Integrated_Model_1.0/main_pd'
 
 #For production version
-export MAIN_S='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/trunk/Integrated_Model_1.0/main_sp'
-export MAIN_P='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/trunk/Integrated_Model_1.0/main_pp'
-echo 'MAIN_S:'$MAIN_S
-echo 'MAIN_P:'$MAIN_P
+#export MAIN_S='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/trunk/Integrated_Model_1.0/main_sp'
+#export MAIN_P='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/trunk/Integrated_Model_1.0/main_pp'
+#echo 'MAIN_S:'$MAIN_S
+#echo 'MAIN_P:'$MAIN_P
 
-
-#Location of the SQL database creation script (not actually used currently):
+#Location of the SQL database creation script (not actually used currently, since gendb.py is now in utils):
 #export SQL_DB_DIR='/media/DOCS/Docs/EURACE/X-models/SVN_linux/xagents/sim_validation'
 
 #Location of the model XML (used to generate the SQL)
-export MODEL_XML_DIR='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/trunk/Integrated_Model_1.0'
+#export MODEL_XML_DIR='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_Model/trunk/Integrated_Model_1.0'
 
 ##UNIBI settings:
 
@@ -90,11 +97,30 @@ export MODEL_XML_DIR='/media/DOCS/Docs/EURACE/X-models/SVN_linux/models/EURACE_M
 #Location of the model XML (used to generate the SQL)
 #export MODEL_XML_DIR='/home/svdhoog/EURACE/SVN/eurace/trunk/Integrated_Model_1.0'
 
+
+##Ubuntu Laptop settings:
+
+#For debug version
+#export MAIN_S='/media/DataStorageLinux/SVN/eurace/trunk/Integrated_Model_1.0/main_sd'
+#export MAIN_P='/media/DataStorageLinux/SVN/eurace/trunk/Integrated_Model_1.0/main_pd'
+
+#For production version
+export MAIN_S='/media/DataStorageLinux/SVN/eurace/trunk/Integrated_Model_1.0/main_sp'
+#export MAIN_S='/media/DataStorageLinux/SVN/eurace/tags/Integrated_Model_1.0beta/main_sp'
+#export MAIN_P='/media/DataStorageLinux/SVN/eurace/trunk/Integrated_Model_1.0/main_pp'
+
+#Location of the SQL database creation script (used after XML output to generate the SQL):
+#export SQL_DB_DIR='/media/DataStorageLinux/SVN/xagents/sim_validation'
+
+#Location of the model XML (used to validate the rules after generating the SQL)
+export MODEL_XML_DIR='/media/DataStorageLinux/SVN/eurace/trunk/Integrated_Model_1.0'
+
 ######### STEP 2: CREATION OF EXPERIMENT FOLDER HIERARCHY 
 bash ./exp_script_1.sh
 
 ######### STEP 3: CREATION OF THE SPECIFIC SETTINGS XML FILE 
-bash ./exp_script_2.sh
+#bash ./exp_script_2_tax.sh
+bash ./exp_script_2_unemployment.sh
 
 ######### STEP 4: RUNNING THE INITIAL PHASE 
 #bash ./run_first_stage.sh
