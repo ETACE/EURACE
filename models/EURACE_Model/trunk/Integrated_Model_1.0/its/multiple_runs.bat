c2330da9563efdf0eec29ee7bedf3b65cc214a8b
raberto <raberto@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-12-19 17:16:36 +0000

    multiple run script

---
+++ models/EURACE_Model/trunk/Integrated_Model_1.0/its/multiple_runs.bat
@@ -0,0 +1,5 @@
+cd E:\research\X-EURACE\models\EURACE_Model\trunk\Integrated_Model_1.0\its\qe1_d1_es0_r1
+main 2400 its\0_expQE.xml -f 4800
+cd E:\research\X-EURACE\models\EURACE_Model\trunk\Integrated_Model_1.0\its\qe1_d1_es0_r2
+main 2400 its\0_expQE.xml -f 4800 
+ 
