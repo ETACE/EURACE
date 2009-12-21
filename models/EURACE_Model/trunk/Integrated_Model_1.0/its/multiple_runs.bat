8dea88153bcf4f0eeee08f5a160d8086d97f8f1b
raberto <raberto@5be612c0-cb41-0410-86ae-e225961b5c23>
2009-12-21 18:15:42 +0000

    bug fix in the wiping out of shareholders in the insolvency bankruptcy case

--- models/EURACE_Model/trunk/Integrated_Model_1.0/its/multiple_runs.bat
+++ models/EURACE_Model/trunk/Integrated_Model_1.0/its/multiple_runs.bat
@@ -1,5 +1,4 @@
-cd C:\DATA\research\X-EURACE\models\EURACE_Model\trunk\Integrated_Model_1.0\its\qe1_d0.5_es0_r1
+cd E:\research\X-EURACE\models\EURACE_Model\trunk\Integrated_Model_1.0\its\qe1_d1_es0_r1
 main 4800 its\0_expQE.xml -f 4800
-cd C:\DATA\research\X-EURACE\models\EURACE_Model\trunk\Integrated_Model_1.0\its\qe1_d0.5_es0_r2
+cd  E:\research\X-EURACE\models\EURACE_Model\trunk\Integrated_Model_1.0\its\qe1_d1_es0_r2
 main 4800 its\0_expQE.xml -f 4800 
- 
