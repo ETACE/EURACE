64e9668b2fe312826db759d9d1d38d39cac4c53d
pharting <pharting@5be612c0-cb41-0410-86ae-e225961b5c23>
2010-01-21 13:22:51 +0000

    Bug fix

--- models/EURACE_Model/branches/UNIBI_Branched_Model_2010/Financial_Management_GREQAM/Firm_Financial_Management_Functions.c
+++ models/EURACE_Model/branches/UNIBI_Branched_Model_2010/Financial_Management_GREQAM/Firm_Financial_Management_Functions.c
@@ -857,7 +857,15 @@ if (BANKRUPTCY_IDLE_COUNTER == CONST_BANKRUPTCY_IDLE_PERIOD - 1)
     {
             add_firing_message(ID, EMPLOYEES.array[i].id);
             remove_employee(&EMPLOYEES, i);
-	}
+    }
+    
+    NO_EMPLOYEES  = 0;
+    NO_EMPLOYEES_SKILL_1= 0;
+    NO_EMPLOYEES_SKILL_2= 0;
+    NO_EMPLOYEES_SKILL_3= 0;
+    NO_EMPLOYEES_SKILL_4= 0;
+    NO_EMPLOYEES_SKILL_5= 0;
+
         
     //Effect on consumption goods market
     //Option 1: all local inventory stock is lost
@@ -900,9 +908,16 @@ int Firm_bankruptcy_illiquidity_procedure()
     for (i=0;i<EMPLOYEES.size;i++)
     {
             add_firing_message(ID, EMPLOYEES.array[i].id);
-            remove_employee(&EMPLOYEES, i);
-	}
-        
+           remove_employee(&EMPLOYEES, i);
+    }
+    
+
+    NO_EMPLOYEES  = 0;
+    NO_EMPLOYEES_SKILL_1= 0;
+    NO_EMPLOYEES_SKILL_2= 0;
+    NO_EMPLOYEES_SKILL_3= 0;
+    NO_EMPLOYEES_SKILL_4= 0;
+    NO_EMPLOYEES_SKILL_5= 0;
     //Effect on consumption goods market
     //Option 1: all local inventory stock is lost
     //Option 2: send back local inventory stock to factory
