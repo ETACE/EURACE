4aeb30283bead91484be44297e233d5d33b6ab57
pharting <pharting@5be612c0-cb41-0410-86ae-e225961b5c23>
2010-01-21 13:30:29 +0000

    Transition phase

--- models/EURACE_Model/branches/UNIBI_Branched_Model_2010/Inv_Goods_UNIBI/IGfirm_functions.c
+++ models/EURACE_Model/branches/UNIBI_Branched_Model_2010/Inv_Goods_UNIBI/IGfirm_functions.c
@@ -35,13 +35,13 @@ int IGFirm_update_productivity_price()
 	
 	
 	
-        //double prod_progress;
+     double prod_progress;
     
     /*If the innovation progress is random and has not a predetermined shape*/
-    /*if(INNOVATION_BY_RANDOM==1)
+    if(INNOVATION_BY_RANDOM==1)
     {
          if(DAY >= TRANSITION_PHASE)
-         {*/
+         {
             
                 i = random_int( 0 ,100);
         
@@ -51,7 +51,7 @@ int IGFirm_update_productivity_price()
                         PRODUCTIVITY = PRODUCTIVITY*(1 + PRODUCTIVITY_PROGRESS);
                         CAPITAL_GOOD_PRICE = CAPITAL_GOOD_PRICE*(1 +PRODUCTIVITY_PROGRESS); 
                 }
-        /*}
+         }
     }
     else//Innovation progress has a predetermined shape
     {
@@ -65,7 +65,7 @@ int IGFirm_update_productivity_price()
             CAPITAL_GOOD_PRICE = CAPITAL_GOOD_PRICE*prod_progress;
             remove_dt_scheme_innovation(&INNOVATION_SCHEME,0);
         }
-    }*/
+    }
 
 
     return 0;
