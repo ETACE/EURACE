e46733c6f8b5c49dbc55cf3c844e66c6419ba920
svdhoog <svdhoog@5be612c0-cb41-0410-86ae-e225961b5c23>
2010-04-27 12:29:17 +0000

    added experiment template files

---
+++ models/EURACE_Model/branches/Experiment_setup_linux/Experiment_multi_parameter/exp_template/exp_script_1.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+######### CREATION OF EXPERIMENT FOLDERS ###############################################
+# Script to create the folder hierarchy
+# - Create folders ./duration_x/intensity_y/frequency_z
+# - Create folders for multiple batch runs: its_"batch"
+# - Copy output.xml and run.sh from top-level to these run folders
+########################################################################################
+BASE=$PWD
+
+echo '  Creating folder hierarchy...'
+echo 'In exp_script_1.sh these values are used:'
+echo 'Duration: [' $F1 ']'
+echo 'Intensity: [' $F2 ']'
+echo 'Frequency: [' $F3 ']'
+
+rm -f STATUS
+mkdir -p 'its'
+cd ./its
+
+for folder1 in $F1; do
+    mkdir -p $folder1
+    echo '    Created folder:' $folder1
+    cd $folder1
+    for folder2 in $F2; do
+        mkdir -p $folder2
+        echo '      Created folder:' $folder1/$folder2
+        cd $folder2
+        for folder3 in $F3; do
+            mkdir -p $folder3
+            echo '        Created folder:' $folder1/$folder2/$folder3
+            #echo $PWD
+            cd $folder3
+            for run in $RUNS; do
+                mkdir -p 'run_'$run
+                echo '          Created folder:' $folder1/$folder2/$folder3/'run_'$run
+                echo $folder1'/'$folder2'/'$folder3'/run_'$run':CREATED'>> $BASE/STATUS
+                cp $BASE/output.xml $BASE/run.sh ./'run_'$run
+            done
+            cd ..
+        done
+        cd ..
+    done
+    cd ..
+done
+cd ..
+
+echo '  Finished creation of folder hierarchy.'
---
+++ models/EURACE_Model/branches/Experiment_setup_linux/Experiment_multi_parameter/exp_template/exp_script_2.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+######### CREATION OF THE SPECIFIC SETTINGS #########################################################
+# Script to create the file specific.xml that contains the specific parameter settings for the cases
+# - Enter folder ./duration_x/intensity_y/frequency_z
+# - Write xml tags and parameter settings to the file specific.xml
+#####################################################################################################
+BASE=$PWD
+
+echo '  Creating specific.xml files in folder hierarchy...'
+echo 'In exp_script_2.sh these values are used:'
+echo 'Duration: [' $F1_values ']'
+echo 'Intensity: [' $F2_values ']'
+echo 'Frequency: [' $F3_values ']'
+
+for f1 in $F1_values; do
+    folder1='duration_'$f1
+    for f2 in $F2_values; do
+        folder2='intensity_'$f2
+        for f3 in $F3_values; do
+            folder3='frequency_'$f3
+                #echo $PWD
+                cd ./its/$folder1/$folder2/$folder3
+                rm -f specific.xml
+                echo '<environment>'>>specific.xml
+
+				#Policy switch
+                echo -n '<policy_exp_energy_shock>'>>specific.xml
+                echo -n '1'>>specific.xml
+                echo '</policy_exp_energy_shock>'>>specific.xml
+
+                #Symmetric shock:
+                echo -n '<symmetric_shock>'>>specific.xml
+                echo -n '1'>>specific.xml
+                echo '</symmetric_shock>'>>specific.xml
+        
+                #Duration:
+                echo -n '<energy_shock_start>'>>specific.xml
+                echo -n '1240'>>specific.xml
+                echo '</energy_shock_start>'>>specific.xml
+        
+                echo -n '<energy_shock_end>'>>specific.xml
+                echo -n $[1240+$f1]>>specific.xml
+                echo '</energy_shock_end>'>>specific.xml
+        
+                #Intensity:
+                echo -n '<const_energy_shock_intensity>'>>specific.xml
+                echo -n $f2>>specific.xml
+                echo '</const_energy_shock_intensity>'>>specific.xml
+                
+                #Frequency:
+                echo -n '<energy_shock_frequency>'>>specific.xml
+                echo -n $f3>>specific.xml
+                echo '</energy_shock_frequency>'>>specific.xml
+                
+                echo '</environment>'>>specific.xml
+                
+                cd $BASE
+        done
+    done
+done
+
+echo '  Finished creation of specific.xml files.'
